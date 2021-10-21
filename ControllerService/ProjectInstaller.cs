5af65d349d45fafd7928cb3c21db0d5855a64ea1
Lesueur Benjamin <benjamin.lesueur@live.com>
2021-10-21 16:23:44 +0200

    Allow Windows service to interact with desktop

--- ControllerService/ProjectInstaller.cs
+++ ControllerService/ProjectInstaller.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.Linq;
+using System.Management;
 using System.Threading.Tasks;
 
 namespace ControllerService
@@ -19,6 +20,17 @@ namespace ControllerService
         protected override void OnAfterInstall(IDictionary savedState)
         {
             base.OnAfterInstall(savedState);
+
+            // The following code ticks the "Allow Windows service to interact with desktop"
+            ConnectionOptions coOptions = new ConnectionOptions();
+            coOptions.Impersonation = ImpersonationLevel.Impersonate;
+            ManagementScope mgmtScope = new ManagementScope(@"root\CIMV2", coOptions);
+            mgmtScope.Connect();
+            ManagementObject wmiService;
+            wmiService = new ManagementObject("Win32_Service.Name='" + serviceInstaller1.ServiceName + "'");
+            ManagementBaseObject InParam = wmiService.GetMethodParameters("Change");
+            InParam["DesktopInteract"] = true;
+            ManagementBaseObject OutParam = wmiService.InvokeMethod("Change", InParam, null);
 
             //The following code starts the services after it is installed.
             using (System.ServiceProcess.ServiceController serviceController = new System.ServiceProcess.ServiceController(serviceInstaller1.ServiceName))
