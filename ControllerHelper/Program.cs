06abb2d0f3317471ad770680dd6841d03f88ba07
Lesueur Benjamin <benjamin.lesueur@live.com>
2021-11-30 08:23:19 +0100

    if application is already running, display it

--- ControllerHelper/Program.cs
+++ ControllerHelper/Program.cs
@@ -3,29 +3,68 @@ using Serilog;
 using Serilog.Events;
 using Serilog.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace ControllerHelper
 {
     static class Program
     {
+        #region imports
+
+        private enum ShowWindowEnum
+        {
+            Hide = 0,
+            ShowNormal = 1, ShowMinimized = 2, ShowMaximized = 3,
+            Maximize = 3, ShowNormalNoActivate = 4, Show = 5,
+            Minimize = 6, ShowMinNoActivate = 7, ShowNoActivate = 8,
+            Restore = 9, ShowDefault = 10, ForceMinimized = 11
+        };
+
+        private struct Windowplacement
+        {
+            public int length;
+            public int flags;
+            public int showCmd;
+            public System.Drawing.Point ptMinPosition;
+            public System.Drawing.Point ptMaxPosition;
+            public System.Drawing.Rectangle rcNormalPosition;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string className, string windowTitle);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, ShowWindowEnum flags);
+        [DllImport("user32.dll")]
+        private static extern int SetForegroundWindow(IntPtr hwnd);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowPlacement(IntPtr hWnd, ref Windowplacement lpwndpl);
+        #endregion
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+            string proc = Process.GetCurrentProcess().ProcessName;
+            Process[] processes = Process.GetProcessesByName(proc);
 
-            string thisprocessname = Process.GetCurrentProcess().ProcessName;
-            if (Process.GetProcesses().Count(p => p.ProcessName == thisprocessname) > 1)
+            if (processes.Length > 1)
             {
-                MessageBox.Show("An instance of Controller Helper is already running.");
+                // get our process
+                BringWindowToFront(proc);
+                // exit our process
                 return;
             }
+
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
 
             var configuration = new ConfigurationBuilder()
                         .AddJsonFile("helpersettings.json")
@@ -41,6 +80,25 @@ namespace ControllerHelper
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ControllerHelper(microsoftLogger));
+        }
+
+        private static void BringWindowToFront(string name)
+        {
+            IntPtr wdwIntPtr = FindWindow(null, name);
+
+            //get the hWnd of the process
+            Windowplacement placement = new Windowplacement();
+            GetWindowPlacement(wdwIntPtr, ref placement);
+
+            // Check if window is minimized
+            if (placement.showCmd == 2)
+            {
+                //the window is hidden so we restore it
+                ShowWindow(wdwIntPtr, ShowWindowEnum.Restore);
+            }
+
+            //set user's focus to the window
+            SetForegroundWindow(wdwIntPtr);
         }
     }
 }
