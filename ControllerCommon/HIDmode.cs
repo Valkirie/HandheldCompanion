c71550aaae63e3d5c6cd670c45e3173977a4fa03
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-01-02 12:47:31 +0100

    update HIDmode

--- ControllerCommon/HIDmode.cs
+++ ControllerCommon/HIDmode.cs
@@ -4,9 +4,9 @@ namespace ControllerCommon
 {
     public enum HIDmode
     {
-        [Description("DualShock 4")]
+        [Description("DualShock 4 emulation")]
         DualShock4Controller = 0,
-        [Description("Xbox 360")]
+        [Description("Xbox 360 emulation")]
         Xbox360Controller = 1,
         [Description("None")]
         None = 2,
