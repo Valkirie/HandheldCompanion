3329289859dfac17a04c1c9d246b4af443adc913
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-01-10 20:13:32 +0100

    0.8.4.2

--- ControllerService/AssemblyInfo1.cs
+++ ControllerService/AssemblyInfo1.cs
@@ -19,5 +19,5 @@
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("0.8.3.4")]
-[assembly: AssemblyFileVersion("0.8.3.4")]
+[assembly: AssemblyVersion("0.8.4.2")]
+[assembly: AssemblyFileVersion("0.8.4.2")]
