1520faf710f57373ca012252a15e4e4dc0f8cab9
CasperH2O <casperhofstede@gmail.com>
2022-01-17 23:28:21 +0100

    Update icon set, support no emulation icon.
    Fixed bug where initial HID mode defaulted to DS logo by switching enum numbering.
    Added application icon for no emulation with service status indicators.

--- ControllerCommon/HIDmode.cs
+++ ControllerCommon/HIDmode.cs
@@ -4,11 +4,11 @@ namespace ControllerCommon
 {
     public enum HIDmode
     {
-        [Description("DualShock 4 emulation")]
-        DualShock4Controller = 0,
-        [Description("Xbox 360 emulation")]
-        Xbox360Controller = 1,
         [Description("No emulation")]
-        None = 2,
+        None = 0,
+        [Description("DualShock 4 emulation")]
+        DualShock4Controller = 1,
+        [Description("Xbox 360 emulation")]
+        Xbox360Controller = 2,
     }
 }
