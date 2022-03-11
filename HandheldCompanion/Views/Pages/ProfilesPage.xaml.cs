62fe5cf8301d3c2ef77fe8c886765bd801ff20e6
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-03-11 12:09:01 +0100

    improve UMC enable buttons look & feel

--- HandheldCompanion/Views/Pages/ProfilesPage.xaml.cs
+++ HandheldCompanion/Views/Pages/ProfilesPage.xaml.cs
@@ -30,7 +30,7 @@ namespace HandheldCompanion.Views.Pages
         PipeClient pipeClient;
 
         // UI vars
-        private int maxColumns = 4;
+        private int maxColumns = 3;
 
         public ProfilesPage()
         {
@@ -62,7 +62,66 @@ namespace HandheldCompanion.Views.Pages
             foreach (GamepadButtonFlags button in (GamepadButtonFlags[])Enum.GetValues(typeof(GamepadButtonFlags)))
             {
                 string description = Utils.GetDescriptionFromEnumValue(button);
-                CheckBox checkbox = new CheckBox() { Tag = button, Content = description };
+
+                SimpleStackPanel panel = new SimpleStackPanel() { Spacing = 6, Orientation = Orientation.Horizontal, VerticalAlignment = VerticalAlignment.Center };
+
+                FontIcon icon = new FontIcon() { FontSize = 24 };
+                switch(button)
+                {
+                    case GamepadButtonFlags.A:
+                        icon.Glyph = "\uF093";
+                        break;
+                    case GamepadButtonFlags.B:
+                        icon.Glyph = "\uF094";
+                        break;
+                    case GamepadButtonFlags.Y:
+                        icon.Glyph = "\uF095";
+                        break;
+                    case GamepadButtonFlags.X:
+                        icon.Glyph = "\uF096";
+                        break;
+                    case GamepadButtonFlags.DPadRight:
+                    case GamepadButtonFlags.DPadDown:
+                    case GamepadButtonFlags.DPadUp:
+                    case GamepadButtonFlags.DPadLeft:
+                        icon.Glyph = "\uF10E";
+                        break;
+                    case GamepadButtonFlags.LeftTrigger:
+                        icon.Glyph = "\uF10A";
+                        break;
+                    case GamepadButtonFlags.RightTrigger:
+                        icon.Glyph = "\uF10B";
+                        break;
+                    case GamepadButtonFlags.LeftShoulder:
+                        icon.Glyph = "\uF10C";
+                        break;
+                    case GamepadButtonFlags.RightShoulder:
+                        icon.Glyph = "\uF10D";
+                        break;
+                    case GamepadButtonFlags.LeftThumb:
+                        icon.Glyph = "\uF108";
+                        break;
+                    case GamepadButtonFlags.RightThumb:
+                        icon.Glyph = "\uF109";
+                        break;
+                    case GamepadButtonFlags.Start:
+                        icon.Glyph = "\uEDE3";      // ButtonMenu
+                        break;
+                    case GamepadButtonFlags.Back:
+                        icon.Glyph = "\uEECA";      // ButtonView2
+                        break;
+                    default:
+                        icon.Glyph = "\uE7E8";
+                        break;
+                }
+
+                if (icon.Glyph != "")
+                    panel.Children.Add(icon);
+
+                TextBlock text = new TextBlock() { Text = description };
+                panel.Children.Add(text);
+
+                CheckBox checkbox = new CheckBox() { Tag = button, Content = panel };
 
                 ((SimpleStackPanel)cB_Buttons.Children[idx]).Children.Add(checkbox);
 
