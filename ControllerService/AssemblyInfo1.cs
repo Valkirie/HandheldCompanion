c914b562f08ac0a06543f0fd3677c1fbaef649c7
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-03-14 17:18:37 +0100

    add prebuild event to Controller Service to kill the service on compilation

--- ControllerService/AssemblyInfo1.cs
+++ ControllerService/AssemblyInfo1.cs
@@ -19,5 +19,5 @@
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("0.9.1.0")]
-[assembly: AssemblyFileVersion("0.9.1.0")]
+[assembly: AssemblyVersion("0.9.1.1")]
+[assembly: AssemblyFileVersion("0.9.1.1")]
