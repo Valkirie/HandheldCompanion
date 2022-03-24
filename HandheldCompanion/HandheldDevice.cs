fc40a4c356c7a247195fed690f477dbc5d332459
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-03-24 10:17:33 +0100

    prevent a crash on null sensor

--- HandheldCompanion/HandheldDevice.cs
+++ HandheldCompanion/HandheldDevice.cs
@@ -9,7 +9,7 @@ namespace HandheldCompanion
 {
     public class HandheldDevice
     {
-        public USBDeviceInfo sensor;
+        public USBDeviceInfo sensor = new USBDeviceInfo("0", "N/A", "");
         public bool sensorSupported;
 
         public string ManufacturerName;
@@ -26,7 +26,22 @@ namespace HandheldCompanion
 
             Gyrometer gyrometer = Gyrometer.GetDefault();
             if (gyrometer != null)
+            {
                 hasGyrometer = true;
+
+                string ACPI = CommonUtils.Between(gyrometer.DeviceId, "ACPI#", "#");
+
+                sensor = GetUSBDevices().FirstOrDefault(device => device.DeviceId.Contains(ACPI));
+                if (sensor != null)
+                {
+                    switch (sensor.Name)
+                    {
+                        case "BMI160":
+                            sensorSupported = true;
+                            break;
+                    }
+                }
+            }
 
             Accelerometer accelerometer = Accelerometer.GetDefault();
             if (accelerometer != null)
@@ -35,21 +50,6 @@ namespace HandheldCompanion
             Inclinometer inclinometer = Inclinometer.GetDefault();
             if (inclinometer != null)
                 hasInclinometer = true;
-
-            Debug.WriteLine("DeviceId: {0}", gyrometer.DeviceId);
-            string ACPI = CommonUtils.Between(gyrometer.DeviceId, "ACPI#", "#");
-
-            sensor = GetUSBDevices().FirstOrDefault(device => device.DeviceId.Contains(ACPI));
-
-            if (sensor != null)
-            {
-                switch (sensor.Name)
-                {
-                    case "BMI160":
-                        sensorSupported = true;
-                        break;
-                }
-            }
         }
     }
 }
