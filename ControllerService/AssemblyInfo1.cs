62883d4c4af6fd0c9772ac75bea46c8e33b197d4
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-04-05 15:20:48 +0200

    build 09420

--- ControllerService/AssemblyInfo1.cs
+++ ControllerService/AssemblyInfo1.cs
@@ -19,5 +19,5 @@
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("0.9.4.1")]
-[assembly: AssemblyFileVersion("0.9.4.1")]
+[assembly: AssemblyVersion("0.9.4.2")]
+[assembly: AssemblyFileVersion("0.9.4.2")]
