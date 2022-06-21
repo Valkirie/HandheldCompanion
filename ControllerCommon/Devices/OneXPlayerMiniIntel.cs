9170ec93171f05c26f25896e19364c11c1a2454c
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-06-21 11:22:25 +0200

    fix OneN Mini Intel (Gundam)

--- ControllerCommon/Devices/OneXPlayerMiniIntel.cs
+++ ControllerCommon/Devices/OneXPlayerMiniIntel.cs
@@ -14,8 +14,7 @@ namespace ControllerCommon.Devices
             this.WidthHeightRatio = 2.4f;
             this.ProductIllustration = "device_onexplayer_mini";
 
-
-            this.AngularVelocityAxis = new Vector3(-1.0f, 1.0f, 1.0f);
+            this.AngularVelocityAxis = new Vector3(1.0f, 1.0f, -1.0f);
             this.AngularVelocityAxisSwap = new()
             {
                 { 'X', 'Y' },
@@ -23,6 +22,7 @@ namespace ControllerCommon.Devices
                 { 'Z', 'X' },
             };
 
+            this.AccelerationAxis = new Vector3(-1.0f, 1.0f, -1.0f);
             this.AccelerationAxisSwap = new()
             {
                 { 'X', 'X' },
