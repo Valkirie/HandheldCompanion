b99b3945a515c17a8be23d2ae63a5273cffd5393
wherewhere <qq2518766683@outlook.com>
2022-06-25 14:13:04 +0800

    Add SettingUI

---
+++ HandheldCompanion/Controls/Setting/Setting.cs
@@ -0,0 +1,178 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+
+namespace HandheldCompanion.Controls
+{
+    [TemplateVisualState(Name = "Normal", GroupName = "CommonStates")]
+    [TemplateVisualState(Name = "Disabled", GroupName = "CommonStates")]
+    [TemplatePart(Name = RightIconPresenter, Type = typeof(Border))]
+    [TemplatePart(Name = PartIconPresenter, Type = typeof(ContentPresenter))]
+    [TemplatePart(Name = PartDescriptionPresenter, Type = typeof(ContentPresenter))]
+    public class Setting : ContentControl
+    {
+        private const string PartIconPresenter = "IconPresenter";
+        private const string PartDescriptionPresenter = "DescriptionPresenter";
+        private const string RightIconPresenter = "ExpandCollapseChevronBorder";
+        private Border _rightIconPresenter;
+        private ContentPresenter _iconPresenter;
+        private ContentPresenter _descriptionPresenter;
+        private Setting _setting;
+
+        public Setting()
+        {
+            DefaultStyleKey = typeof(Setting);
+        }
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+           "Header",
+           typeof(string),
+           typeof(Setting),
+           new PropertyMetadata(default(string), OnHeaderChanged));
+
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+            "Description",
+            typeof(object),
+            typeof(Setting),
+            new PropertyMetadata(null, OnDescriptionChanged));
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            "Icon",
+            typeof(object),
+            typeof(Setting),
+            new PropertyMetadata(default(string), OnIconChanged));
+
+        public static readonly DependencyProperty RightIconProperty = DependencyProperty.Register(
+            "RightIcon",
+            typeof(string),
+            typeof(Setting),
+            new PropertyMetadata(null, OnIconChanged));
+
+        public static readonly DependencyProperty ActionContentProperty = DependencyProperty.Register(
+            "ActionContent",
+            typeof(object),
+            typeof(Setting),
+            null);
+
+        [Localizable(true)]
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        [Localizable(true)]
+        public object Description
+        {
+            get => GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public object Icon
+        {
+            get => GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public string RightIcon
+        {
+            get => (string)GetValue(RightIconProperty);
+            set => SetValue(RightIconProperty, value);
+        }
+
+        public object ActionContent
+        {
+            get => GetValue(ActionContentProperty);
+            set => SetValue(ActionContentProperty, value);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            IsEnabledChanged -= Setting_IsEnabledChanged;
+            _setting = this;
+            _rightIconPresenter = (Border)_setting.GetTemplateChild(RightIconPresenter);
+            _iconPresenter = (ContentPresenter)_setting.GetTemplateChild(PartIconPresenter);
+            _descriptionPresenter = (ContentPresenter)_setting.GetTemplateChild(PartDescriptionPresenter);
+            Update();
+            SetEnabledState();
+            IsEnabledChanged += Setting_IsEnabledChanged;
+            base.OnApplyTemplate();
+        }
+
+        private static void OnHeaderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((Setting)d).Update();
+        }
+
+        private static void OnIconChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((Setting)d).Update();
+        }
+
+        private static void OnDescriptionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((Setting)d).Update();
+        }
+
+        private void Setting_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            SetEnabledState();
+        }
+
+        private void SetEnabledState()
+        {
+            VisualStateManager.GoToState(this, IsEnabled ? "Normal" : "Disabled", true);
+        }
+
+        private void Update()
+        {
+            if (_setting == null)
+            {
+                return;
+            }
+
+            if (_setting.ActionContent != null)
+            {
+                if (_setting.ActionContent.GetType() != typeof(Button))
+                {
+                    // We do not want to override the default AutomationProperties.Name of a button. Its Content property already describes what it does.
+                    if (!string.IsNullOrEmpty(_setting.Header))
+                    {
+                        AutomationProperties.SetName((UIElement)_setting.ActionContent, _setting.Header);
+                    }
+                }
+            }
+
+            if (_setting._iconPresenter != null)
+            {
+                if (_setting.Icon == null)
+                {
+                    _setting._iconPresenter.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    _setting._iconPresenter.Visibility = Visibility.Visible;
+                }
+            }
+
+            if (_setting.RightIcon == null)
+            {
+                _setting._rightIconPresenter.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                _setting._rightIconPresenter.Visibility = Visibility.Visible;
+            }
+
+            if (_setting.Description == null)
+            {
+                _setting._descriptionPresenter.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                _setting._descriptionPresenter.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ HandheldCompanion/Controls/SettingsGroup/SettingsGroup.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+
+namespace HandheldCompanion.Controls
+{
+    [TemplateVisualState(Name = "Normal", GroupName = "CommonStates")]
+    [TemplateVisualState(Name = "Disabled", GroupName = "CommonStates")]
+    public partial class SettingsGroup : ItemsControl
+    {
+        public SettingsGroup()
+        {
+            DefaultStyleKey = typeof(SettingsGroup);
+        }
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+            "Header",
+            typeof(string),
+            typeof(SettingsGroup),
+            new PropertyMetadata(default(string)));
+
+        [Localizable(true)]
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            IsEnabledChanged -= SettingsGroup_IsEnabledChanged;
+            SetEnabledState();
+            IsEnabledChanged += SettingsGroup_IsEnabledChanged;
+            base.OnApplyTemplate();
+        }
+
+        private void SettingsGroup_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            SetEnabledState();
+        }
+
+        private void SetEnabledState()
+        {
+            VisualStateManager.GoToState(this, IsEnabled ? "Normal" : "Disabled", true);
+        }
+
+        protected override AutomationPeer OnCreateAutomationPeer()
+        {
+            return new SettingsGroupAutomationPeer(this);
+        }
+    }
+
+    public class SettingsGroupAutomationPeer : FrameworkElementAutomationPeer
+    {
+        public SettingsGroupAutomationPeer(SettingsGroup owner)
+            : base(owner)
+        {
+        }
+
+        protected override string GetNameCore()
+        {
+            SettingsGroup? selectedSettingsGroup = (SettingsGroup)Owner;
+            return selectedSettingsGroup.Header;
+        }
+    }
+}
