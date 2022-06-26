c130fe505d8458e16d808b3d9ebd303541698933
CasperH <casperhofstede@gmail.com>
2022-06-26 16:05:07 +0200

    GPDWinMax2 support with verified IMU mounting.

---
+++ ControllerCommon/Devices/GPDWinMax2.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+using WindowsInput.Events;
+using static ControllerCommon.OneEuroFilter;
+
+namespace ControllerCommon.Devices
+{
+    public class GPDWinMax2 : Device
+    {
+        public GPDWinMax2() : base()
+        {
+            this.ProductSupported = true;
+
+            // device specific settings
+            this.WidthHeightRatio = 2.4f;
+            this.ProductIllustration = "device_gpd_winmax2";
+
+            this.AngularVelocityAxis = new Vector3(1.0f, -1.0f, 1.0f);
+            this.AngularVelocityAxisSwap = new()
+            {
+                { 'X', 'X' },
+                { 'Y', 'Z' },
+                { 'Z', 'Y' },
+            };
+
+            this.AccelerationAxis = new Vector3(1.0f, 1.0f, 1.0f);
+            this.AccelerationAxisSwap = new()
+            {
+                { 'X', 'X' },
+                { 'Y', 'Z' },
+                { 'Z', 'Y' },
+            };
+        }
+    }
+}
