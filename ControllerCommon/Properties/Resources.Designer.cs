a870307842f28534af562004a5639c768bcaf36f
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-07-27 15:05:29 +0200

    fix an issue preventing profiles from being copied to %mydocuments% if missing

--- ControllerCommon/Properties/Resources.Designer.cs
+++ ControllerCommon/Properties/Resources.Designer.cs
@@ -61,53 +61,6 @@ namespace ControllerCommon.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to {
-        ///  &quot;name&quot;: &quot;AYASpace&quot;,
-        ///  &quot;path&quot;: &quot;AYASpace.exe&quot;,
-        ///  &quot;executable&quot;: &quot;AYASpace.exe&quot;,
-        ///  &quot;isEnabled&quot;: true,
-        ///  &quot;whitelisted&quot;: false
-        ///}.
-        /// </summary>
-        public static string AYASpace_json {
-            get {
-                return ResourceManager.GetString("AYASpace.json", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to {
-        ///  &quot;name&quot;: &quot;ControllerCompanion&quot;,
-        ///  &quot;path&quot;: &quot;ControllerCompanion.exe&quot;,
-        ///  &quot;executable&quot;: &quot;ControllerCompanion.exe&quot;,
-        ///  &quot;isEnabled&quot;: true,
-        ///  &quot;whitelisted&quot;: true
-        ///}.
-        /// </summary>
-        public static string ControllerCompanion_json {
-            get {
-                return ResourceManager.GetString("ControllerCompanion.json", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to {
-        ///  &quot;name&quot;: &quot;Default&quot;,
-        ///  &quot;path&quot;: &quot;&quot;,
-        ///  &quot;executable&quot;: &quot;Default.exe&quot;,
-        ///  &quot;isDefault&quot;: true,
-        ///  &quot;isEnabled&quot;: true,
-        ///  &quot;whitelisted&quot;: false,
-        ///  &quot;umc_enabled&quot;: false
-        ///}.
-        /// </summary>
-        public static string Default_json {
-            get {
-                return ResourceManager.GetString("Default.json", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Button A.
         /// </summary>
         public static string Enum_GamepadButtonFlagsExt_A {
@@ -450,57 +403,11 @@ namespace ControllerCommon.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to {
-        ///  &quot;name&quot;: &quot;Joyxoff&quot;,
-        ///  &quot;path&quot;: &quot;Joyxoff.exe&quot;,
-        ///  &quot;executable&quot;: &quot;Joyxoff.exe&quot;,
-        ///  &quot;isEnabled&quot;: true,
-        ///  &quot;whitelisted&quot;: true
-        ///}.
-        /// </summary>
-        public static string JoyXOff_json {
-            get {
-                return ResourceManager.GetString("JoyXOff.json", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to This input will operate as a joystick optimized for controlling a first or third person camera.
         /// </summary>
         public static string PlayerSpaceDesc {
             get {
                 return ResourceManager.GetString("PlayerSpaceDesc", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to {
-        ///  &quot;name&quot;: &quot;Playnite.FullscreenApp&quot;,
-        ///  &quot;path&quot;: &quot;Playnite.FullscreenApp.exe&quot;,
-        ///  &quot;executable&quot;: &quot;Playnite.FullscreenApp.exe&quot;,
-        ///  &quot;isEnabled&quot;: true,
-        ///  &quot;whitelisted&quot;: true
-        ///}.
-        /// </summary>
-        public static string Playnite_FullscreenApp_json {
-            get {
-                return ResourceManager.GetString("Playnite.FullscreenApp.json", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to {
-        ///  &quot;name&quot;: &quot;Power Control Panel&quot;,
-        ///  &quot;path&quot;: &quot;Power Control Panel.exe&quot;,
-        ///  &quot;executable&quot;: &quot;Power Control Panel.exe&quot;,
-        ///  &quot;isEnabled&quot;: true,
-        ///  &quot;whitelisted&quot;: true
-        ///}
-        ///.
-        /// </summary>
-        public static string PowerControlPanel_json {
-            get {
-                return ResourceManager.GetString("PowerControlPanel.json", resourceCulture);
             }
         }
     }
--- HandheldCompanion/Properties/Resources.Designer.cs
+++ HandheldCompanion/Properties/Resources.Designer.cs
@@ -241,6 +241,21 @@ namespace HandheldCompanion.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to {
+        ///  &quot;name&quot;: &quot;ControllerCompanion&quot;,
+        ///  &quot;path&quot;: &quot;ControllerCompanion.exe&quot;,
+        ///  &quot;executable&quot;: &quot;ControllerCompanion.exe&quot;,
+        ///  &quot;isEnabled&quot;: true,
+        ///  &quot;whitelisted&quot;: true
+        ///}.
+        /// </summary>
+        public static string ControllerCompanion_json {
+            get {
+                return ResourceManager.GetString("ControllerCompanion.json", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Cloak device.
         /// </summary>
         public static string ControllerPage_CloakDevice {
@@ -340,6 +355,23 @@ namespace HandheldCompanion.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to {
+        ///  &quot;name&quot;: &quot;Default&quot;,
+        ///  &quot;path&quot;: &quot;&quot;,
+        ///  &quot;executable&quot;: &quot;Default.exe&quot;,
+        ///  &quot;isDefault&quot;: true,
+        ///  &quot;isEnabled&quot;: true,
+        ///  &quot;whitelisted&quot;: false,
+        ///  &quot;umc_enabled&quot;: false
+        ///}.
+        /// </summary>
+        public static string Default_json {
+            get {
+                return ResourceManager.GetString("Default.json", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to 3D controller hotkey.
         /// </summary>
         public static string HotkeysPage_OverlayModel {
@@ -408,6 +440,21 @@ namespace HandheldCompanion.Properties {
         public static string HotkeysPage_SuspenderDesc {
             get {
                 return ResourceManager.GetString("HotkeysPage_SuspenderDesc", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {
+        ///  &quot;name&quot;: &quot;Joyxoff&quot;,
+        ///  &quot;path&quot;: &quot;Joyxoff.exe&quot;,
+        ///  &quot;executable&quot;: &quot;Joyxoff.exe&quot;,
+        ///  &quot;isEnabled&quot;: true,
+        ///  &quot;whitelisted&quot;: true
+        ///}.
+        /// </summary>
+        public static string JoyXOff_json {
+            get {
+                return ResourceManager.GetString("JoyXOff.json", resourceCulture);
             }
         }
         
@@ -858,6 +905,37 @@ namespace HandheldCompanion.Properties {
         public static string OverlayPage_ZDOPlusController {
             get {
                 return ResourceManager.GetString("OverlayPage_ZDOPlusController", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {
+        ///  &quot;name&quot;: &quot;Playnite.FullscreenApp&quot;,
+        ///  &quot;path&quot;: &quot;Playnite.FullscreenApp.exe&quot;,
+        ///  &quot;executable&quot;: &quot;Playnite.FullscreenApp.exe&quot;,
+        ///  &quot;isEnabled&quot;: true,
+        ///  &quot;whitelisted&quot;: true
+        ///}.
+        /// </summary>
+        public static string Playnite_FullscreenApp_json {
+            get {
+                return ResourceManager.GetString("Playnite.FullscreenApp.json", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {
+        ///  &quot;name&quot;: &quot;Power Control Panel&quot;,
+        ///  &quot;path&quot;: &quot;Power Control Panel.exe&quot;,
+        ///  &quot;executable&quot;: &quot;Power Control Panel.exe&quot;,
+        ///  &quot;isEnabled&quot;: true,
+        ///  &quot;whitelisted&quot;: true
+        ///}
+        ///.
+        /// </summary>
+        public static string PowerControlPanel_json {
+            get {
+                return ResourceManager.GetString("PowerControlPanel.json", resourceCulture);
             }
         }
         
