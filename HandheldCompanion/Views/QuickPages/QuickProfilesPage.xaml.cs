8048eedf36cd98bf1d74f7b95430b8f0945e1fc9
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-08-05 21:55:19 +0200

    define profile tdp values when override is toggled

--- HandheldCompanion/Views/QuickPages/QuickProfilesPage.xaml.cs
+++ HandheldCompanion/Views/QuickPages/QuickProfilesPage.xaml.cs
@@ -270,6 +270,8 @@ namespace HandheldCompanion.Views.QuickPages
 
             // Power settings
             currentProfile.TDP_override = (bool)TDPToggle.IsOn;
+            currentProfile.TDP_value[0] = (int)TDPSustainedSlider.Value;
+            currentProfile.TDP_value[1] = (int)TDPBoostSlider.Value;
         }
 
         private void TDPSustainedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
