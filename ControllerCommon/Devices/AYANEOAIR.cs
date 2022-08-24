8525776ddf611388f88a69282f0aa9cb1b7476c7
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-08-24 13:18:35 +0200

    improve AYA NEO Air custom keys compatibility.
    We've got all four custom buttons covered

--- ControllerCommon/Devices/AYANEOAIR.cs
+++ ControllerCommon/Devices/AYANEOAIR.cs
@@ -33,12 +33,10 @@ namespace ControllerCommon.Devices
                 { 'Z', 'Y' },
             };
 
-            listeners.Add(new DeviceChord("Top Left Long", new List<KeyCode>() { KeyCode.RControlKey, KeyCode.LWin, KeyCode.RControlKey, KeyCode.RAlt, KeyCode.Delete }));
-            listeners.Add(new DeviceChord("Top Left Short", new List<KeyCode>() { KeyCode.RControlKey, KeyCode.LWin }));
-            listeners.Add(new DeviceChord("Top Right Long", new List<KeyCode>() { KeyCode.RControlKey, KeyCode.LWin, KeyCode.LWin, KeyCode.Tab }));
-            listeners.Add(new DeviceChord("Top Right Short", new List<KeyCode>() { KeyCode.RControlKey, KeyCode.LWin, KeyCode.Escape }));
-            listeners.Add(new DeviceChord("Custom Key Small", new List<KeyCode>() { KeyCode.LWin, KeyCode.LWin, KeyCode.D }));
-            listeners.Add(new DeviceChord("Custom Key Big", new List<KeyCode>() { KeyCode.LControlKey, KeyCode.LWin }));
+            listeners.Add(new DeviceChord("Custom Key Top Right", new List<KeyCode>() { KeyCode.RControlKey, KeyCode.LWin, KeyCode.F10 }));
+            listeners.Add(new DeviceChord("Custom Key Top Left", new List<KeyCode>() { KeyCode.RControlKey, KeyCode.LWin, KeyCode.F11 }));
+            listeners.Add(new DeviceChord("Custom Key Big", new List<KeyCode>() { KeyCode.RControlKey, KeyCode.LWin, KeyCode.F12 }));
+            listeners.Add(new DeviceChord("Custom Key Small", new List<KeyCode>() { KeyCode.LWin, KeyCode.D }));
         }
     }
 }
