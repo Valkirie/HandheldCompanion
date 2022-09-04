83b7eb35e604eaa639e816b58e2f9cff0a29b079
CasperH <casperhofstede@gmail.com>
2022-09-04 20:12:53 +0200

    GPD WinMax2 AMD IMU Addition

---
+++ ControllerCommon/Devices/GPDWinMax2AMD.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace ControllerCommon.Devices
+{
+    public class GPDWinMax2AMD : Device
+    {
+        public GPDWinMax2AMD() : base()
+        {
+            this.ProductSupported = true;
+
+            // device specific settings
+            this.WidthHeightRatio = 2.4f;
+            this.ProductIllustration = "device_gpd_winmax2";
+
+            // https://www.amd.com/fr/products/apu/amd-ryzen-7-6800u
+            this.nTDP = new double[] { 15, 15, 28 };
+            this.cTDP = new double[] { 15, 28 };
+
+            this.AngularVelocityAxis = new Vector3(1.0f, 1.0f, -1.0f);
+            this.AngularVelocityAxisSwap = new()
+            {
+                { 'X', 'Y' },
+                { 'Y', 'Z' },
+                { 'Z', 'X' },
+            };
+
+            this.AccelerationAxis = new Vector3(1.0f, -1.0f, 1.0f);
+            this.AccelerationAxisSwap = new()
+            {
+                { 'X', 'X' },
+                { 'Y', 'Z' },
+                { 'Z', 'Y' },
+            };
+        }
+    }
+}
