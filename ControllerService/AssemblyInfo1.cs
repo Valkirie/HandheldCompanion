68bc116c1b9b1a8c0d535bbcd46b2b902856ce17
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-11-03 09:47:44 +0100

    build 0.12.3.1

--- ControllerService/AssemblyInfo1.cs
+++ ControllerService/AssemblyInfo1.cs
@@ -19,5 +19,5 @@
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("0.12.3.0")]
-[assembly: AssemblyFileVersion("0.12.3.0")]
+[assembly: AssemblyVersion("0.12.3.1")]
+[assembly: AssemblyFileVersion("0.12.3.1")]
