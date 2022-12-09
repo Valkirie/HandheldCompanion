5ad83401938eaae469344e782d9c0c8a080a52a9
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-12-09 14:29:07 +0100

    more work

--- ControllerCommon/Controllers/NetpuneController.cs
+++ ControllerCommon/Controllers/NetpuneController.cs
@@ -1,5 +1,6 @@
 ﻿using neptune_hidapi.net;
 using SharpDX.DirectInput;
+using SharpDX.XInput;
 using System;
 using System.Threading.Tasks;
 
@@ -8,8 +9,8 @@ namespace ControllerCommon.Controllers
     public class NetpuneController : IController
     {
         private NeptuneController Controller = new();
-        private JoystickState State = new();
-        private JoystickState prevState = new();
+        private NeptuneControllerInputState State;
+        private NeptuneControllerInputState prevState;
 
         private bool isConnected = false;
 
@@ -21,17 +22,11 @@ namespace ControllerCommon.Controllers
 
         public override string ToString()
         {
-            // localize me
-            return "Steam Deck Controller";
+            return Details.DeviceDesc;
         }
 
         public override void UpdateReport()
         {
-            // skip if controller isn't connected
-            if (!IsConnected())
-                return;
-
-            base.UpdateReport();
         }
 
         public override bool IsConnected()
@@ -41,6 +36,14 @@ namespace ControllerCommon.Controllers
 
         public override async void Rumble()
         {
+            for (int i = 0; i < 2; i++)
+            {
+                SetVibration(ushort.MaxValue, ushort.MaxValue);
+                await Task.Delay(100);
+
+                SetVibration(0, 0);
+                await Task.Delay(100);
+            }
             base.Rumble();
         }
 
@@ -64,6 +67,96 @@ namespace ControllerCommon.Controllers
 
         private void OnControllerInputReceived(NeptuneControllerInputEventArgs input)
         {
+            if (prevState.GetHashCode() == State.GetHashCode())
+                return;
+
+            Inputs.Buttons = InjectedButtons;
+
+            if (input.State.ButtonState[NeptuneControllerButton.BtnA])
+                Inputs.Buttons |= ControllerButtonFlags.B1;
+            if (input.State.ButtonState[NeptuneControllerButton.BtnB])
+                Inputs.Buttons |= ControllerButtonFlags.B2;
+            if (input.State.ButtonState[NeptuneControllerButton.BtnX])
+                Inputs.Buttons |= ControllerButtonFlags.B3;
+            if (input.State.ButtonState[NeptuneControllerButton.BtnY])
+                Inputs.Buttons |= ControllerButtonFlags.B4;
+
+            if (input.State.ButtonState[NeptuneControllerButton.BtnOptions])
+                Inputs.Buttons |= ControllerButtonFlags.Start;
+            if (input.State.ButtonState[NeptuneControllerButton.BtnMenu])
+                Inputs.Buttons |= ControllerButtonFlags.Back;
+
+            if (input.State.ButtonState[NeptuneControllerButton.BtnSteam])
+                Inputs.Buttons |= ControllerButtonFlags.Special;
+            if (input.State.ButtonState[NeptuneControllerButton.BtnQuickAccess])
+                Inputs.Buttons |= ControllerButtonFlags.OEM1;
+
+            if (input.State.AxesState[NeptuneControllerAxis.L2] > Gamepad.TriggerThreshold)
+                Inputs.Buttons |= ControllerButtonFlags.LeftTrigger;
+            if (input.State.AxesState[NeptuneControllerAxis.R2] > Gamepad.TriggerThreshold)
+                Inputs.Buttons |= ControllerButtonFlags.RightTrigger;
+
+            if (input.State.ButtonState[NeptuneControllerButton.BtnLStickPress])
+                Inputs.Buttons |= ControllerButtonFlags.LeftThumb;
+            if (input.State.ButtonState[NeptuneControllerButton.BtnRStickPress])
+                Inputs.Buttons |= ControllerButtonFlags.RightThumb;
+
+            if (input.State.ButtonState[NeptuneControllerButton.BtnLStickTouch])
+                Inputs.Buttons |= ControllerButtonFlags.OEM2;
+            if (input.State.ButtonState[NeptuneControllerButton.BtnRStickTouch])
+                Inputs.Buttons |= ControllerButtonFlags.OEM3;
+
+            if (input.State.ButtonState[NeptuneControllerButton.BtnL1])
+                Inputs.Buttons |= ControllerButtonFlags.LeftShoulder;
+            if (input.State.ButtonState[NeptuneControllerButton.BtnR1])
+                Inputs.Buttons |= ControllerButtonFlags.RightShoulder;
+
+            if (input.State.ButtonState[NeptuneControllerButton.BtnDpadUp])
+                Inputs.Buttons |= ControllerButtonFlags.DPadUp;
+            if (input.State.ButtonState[NeptuneControllerButton.BtnDpadDown])
+                Inputs.Buttons |= ControllerButtonFlags.DPadDown;
+            if (input.State.ButtonState[NeptuneControllerButton.BtnDpadLeft])
+                Inputs.Buttons |= ControllerButtonFlags.DPadLeft;
+            if (input.State.ButtonState[NeptuneControllerButton.BtnDpadRight])
+                Inputs.Buttons |= ControllerButtonFlags.DPadRight;
+
+            // Left Stick
+            if (input.State.AxesState[NeptuneControllerAxis.LeftStickX] < -Gamepad.LeftThumbDeadZone)
+                Inputs.Buttons |= ControllerButtonFlags.LStickLeft;
+            if (input.State.AxesState[NeptuneControllerAxis.LeftStickX] > Gamepad.LeftThumbDeadZone)
+                Inputs.Buttons |= ControllerButtonFlags.LStickRight;
+
+            if (input.State.AxesState[NeptuneControllerAxis.LeftStickY] < -Gamepad.LeftThumbDeadZone)
+                Inputs.Buttons |= ControllerButtonFlags.LStickDown;
+            if (input.State.AxesState[NeptuneControllerAxis.LeftStickY] > Gamepad.LeftThumbDeadZone)
+                Inputs.Buttons |= ControllerButtonFlags.LStickUp;
+
+            Inputs.LeftThumbX = input.State.AxesState[NeptuneControllerAxis.LeftStickX];
+            Inputs.LeftThumbY = input.State.AxesState[NeptuneControllerAxis.LeftStickY];
+
+            // Right Stick
+            if (input.State.AxesState[NeptuneControllerAxis.RightStickX] < -Gamepad.LeftThumbDeadZone)
+                Inputs.Buttons |= ControllerButtonFlags.RStickLeft;
+            if (input.State.AxesState[NeptuneControllerAxis.RightStickX] > Gamepad.LeftThumbDeadZone)
+                Inputs.Buttons |= ControllerButtonFlags.RStickRight;
+
+            if (input.State.AxesState[NeptuneControllerAxis.RightStickY] < -Gamepad.LeftThumbDeadZone)
+                Inputs.Buttons |= ControllerButtonFlags.RStickDown;
+            if (input.State.AxesState[NeptuneControllerAxis.RightStickY] > Gamepad.LeftThumbDeadZone)
+                Inputs.Buttons |= ControllerButtonFlags.RStickUp;
+
+            Inputs.RightThumbX = input.State.AxesState[NeptuneControllerAxis.RightStickX];
+            Inputs.RightThumbY = input.State.AxesState[NeptuneControllerAxis.RightStickY];
+
+            Inputs.LeftTrigger = input.State.AxesState[NeptuneControllerAxis.L2];
+            Inputs.RightTrigger = input.State.AxesState[NeptuneControllerAxis.R2];
+
+            // todo: map trackpad(s)
+
+            // update states
+            prevState = State;
+
+            base.UpdateReport();
         }
 
         public override void Unplug()
@@ -82,6 +175,17 @@ namespace ControllerCommon.Controllers
             base.Unplug();
         }
 
+        public override void SetVibration(ushort LargeMotor, ushort SmallMotor)
+        {
+            ushort LeftMotorSpeed = (ushort)((LargeMotor * ushort.MaxValue / byte.MaxValue) * VibrationStrength);
+            ushort RightMotorSpeed = (ushort)((SmallMotor * ushort.MaxValue / byte.MaxValue) * VibrationStrength);
+
+            // most likely incorrect
+            // todo: https://github.com/mKenfenheuer/steam-deck-windows-usermode-driver/blob/69ce8085d3b6afe888cb2e36bd95836cea58084a/SWICD/Services/ControllerService.cs
+            Controller.SetHaptic(1, LeftMotorSpeed, 30, 0);
+            Controller.SetHaptic(0, RightMotorSpeed, 30, 0);
+        }
+
         private void OnServerMessage(PipeMessage message)
         {
             switch (message.code)
@@ -89,9 +193,7 @@ namespace ControllerCommon.Controllers
                 case PipeCode.SERVER_VIBRATION:
                     {
                         PipeClientVibration e = (PipeClientVibration)message;
-
-                        ushort LeftMotorSpeed = (ushort)((e.LargeMotor * ushort.MaxValue / byte.MaxValue) * VibrationStrength);
-                        ushort RightMotorSpeed = (ushort)((e.SmallMotor * ushort.MaxValue / byte.MaxValue) * VibrationStrength);
+                        SetVibration(e.LargeMotor, e.SmallMotor);
                     }
                     break;
             }
