f3b3278fe53b5cd202fc26ad7ffd2fcb1a909139
Lesueur Benjamin <benjamin.lesueur@live.com>
2022-12-27 13:09:21 +0100

    prevent a race condition with PlatformManager

--- HandheldCompanion/Managers/PlatformManager.cs
+++ HandheldCompanion/Managers/PlatformManager.cs
@@ -7,14 +7,20 @@ namespace HandheldCompanion.Managers
     public static class PlatformManager
     {
         private static SteamPlatform steamPlatform;
+        private static bool IsInitialized;
 
         public static void Start()
         {
             steamPlatform = new();
+
+            IsInitialized = true;
         }
 
         public static Platform GetPlatform(Process proc)
         {
+            if (!IsInitialized)
+                return Platform.None;
+
             if (steamPlatform.IsRelated(proc))
                 return steamPlatform.Platform;
 
