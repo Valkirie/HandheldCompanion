47c32d7148bc5993f0f52cf0a730f5d485c8749c
Lesueur Benjamin <benjamin.lesueur@live.com>
2023-04-17 08:48:50 +0200

    implement Default Device F1-F4 OEM key support
    helpful for debugging...

--- ControllerCommon/Devices/DefaultDevice.cs
+++ ControllerCommon/Devices/DefaultDevice.cs
@@ -1,9 +1,36 @@
-﻿namespace ControllerCommon.Devices
+﻿using ControllerCommon.Inputs;
+using System.Collections.Generic;
+using WindowsInput.Events;
+
+namespace ControllerCommon.Devices
 {
     public class DefaultDevice : IDevice
     {
         public DefaultDevice() : base()
         {
+            OEMChords.Add(new DeviceChord("F1",
+                new List<KeyCode>() { KeyCode.F1 },
+                new List<KeyCode>() { KeyCode.F1 },
+                false, ButtonFlags.OEM1
+                ));
+
+            OEMChords.Add(new DeviceChord("F2",
+                new List<KeyCode>() { KeyCode.F2 },
+                new List<KeyCode>() { KeyCode.F2 },
+                false, ButtonFlags.OEM2
+                ));
+
+            OEMChords.Add(new DeviceChord("F3",
+                new List<KeyCode>() { KeyCode.F3 },
+                new List<KeyCode>() { KeyCode.F3 },
+                false, ButtonFlags.OEM3
+                ));
+
+            OEMChords.Add(new DeviceChord("F4",
+                new List<KeyCode>() { KeyCode.F4 },
+                new List<KeyCode>() { KeyCode.F4 },
+                false, ButtonFlags.OEM4
+                ));
         }
     }
 }
