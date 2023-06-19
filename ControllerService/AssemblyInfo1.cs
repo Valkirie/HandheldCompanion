947cf7c77918c102187469011018adabc631d932
Lesueur Benjamin <benjamin.lesueur@live.com>
2023-06-19 18:27:27 +0200

    Release 0.16.1.2

--- ControllerService/AssemblyInfo1.cs
+++ ControllerService/AssemblyInfo1.cs
@@ -21,5 +21,5 @@
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("0.16.1.1")]
-[assembly: AssemblyFileVersion("0.16.1.1")]
+[assembly: AssemblyVersion("0.16.1.2")]
+[assembly: AssemblyFileVersion("0.16.1.2")]
