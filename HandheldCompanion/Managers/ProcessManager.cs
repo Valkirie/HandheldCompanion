b38bec5c3b96918f37271147928fcaf3d7bb5200
CasperH2O <CasperH2O@users.noreply.github.com>
2023-06-29 15:53:45 +0200

    More ignore list additions (#665)
    * Additional processes to ignore.
    
    * Fix .exe

--- HandheldCompanion/Managers/ProcessManager.cs
+++ HandheldCompanion/Managers/ProcessManager.cs
@@ -415,6 +415,9 @@ public static class ProcessManager
             case "searchapp.exe":
             case "ashotplugctrl.exe":
             case "gameinputsvc.exe":
+            case "gamebuzz.exe":
+            case "asmultidisplaycontrol.exe":
+            case "lockapp.exe":
                 return ProcessFilter.Desktop;
 
             default:
