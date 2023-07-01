a4f2ffd188aaae5b95a1291f7fed689128cc1610
Lesueur Benjamin <benjamin.lesueur@live.com>
2023-07-01 14:09:09 +0200

    Fix Core Isolation, Memory Integrity checks

--- HandheldCompanion/Managers/PerformanceManager.cs
+++ HandheldCompanion/Managers/PerformanceManager.cs
@@ -622,19 +622,14 @@ public class PerformanceManager : Manager
         // initialize processor
         processor = Processor.GetCurrent();
 
-        // higher interval on Intel CPUs to avoid CPU overload
-        if (processor.GetType() == typeof(IntelProcessor))
-        {
-            // read OS specific values
-            var HypervisorEnforcedCodeIntegrityEnabled = RegistryUtils.GetBoolean(
-                @"SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity", "Enabled");
-            var VulnerableDriverBlocklistEnable =
-                RegistryUtils.GetBoolean(@"SYSTEM\CurrentControlSet\Control\CI\Config",
-                    "VulnerableDriverBlocklistEnable");
+        // read OS specific values
+        var HypervisorEnforcedCodeIntegrityEnabled = RegistryUtils.GetBoolean(@"SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios",
+            "HypervisorEnforcedCodeIntegrity");
+        var VulnerableDriverBlocklistEnable = RegistryUtils.GetBoolean(@"SYSTEM\CurrentControlSet\Control\CI\Config",
+            "VulnerableDriverBlocklistEnable");
 
-            if (VulnerableDriverBlocklistEnable || HypervisorEnforcedCodeIntegrityEnabled)
-                LogManager.LogWarning("Core isolation settings are turned on. TDP read/write is disabled");
-        }
+        if (VulnerableDriverBlocklistEnable || HypervisorEnforcedCodeIntegrityEnabled)
+            LogManager.LogWarning("Core isolation settings are turned on. TDP read/write and fan control might be disabled");
 
         if (processor.IsInitialized)
         {
