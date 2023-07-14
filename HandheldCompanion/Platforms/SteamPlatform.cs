07317be445744e4563fd061d9370af0318e23b24
Lesueur Benjamin <benjamin.lesueur@live.com>
2023-07-14 18:31:02 +0200

    Steam fixes inspired by @Havner's work https://github.com/Havner/HandheldCompanion/commit/2b364f56451e27684ee8f1ecc1a70a7c7b4a45e9

--- HandheldCompanion/Platforms/SteamPlatform.cs
+++ HandheldCompanion/Platforms/SteamPlatform.cs
@@ -4,15 +4,20 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using ControllerCommon.Managers;
 using ControllerCommon.Platforms;
 using ControllerCommon.Utils;
+using HandheldCompanion.Controls;
+using HandheldCompanion.Managers;
 using HandheldCompanion.Properties;
 
 namespace HandheldCompanion.Platforms;
 
 public class SteamPlatform : IPlatform
 {
+    private string RunningName;
+
     private static readonly Regex ControllerBlacklistRegex =
         new("^(\\s*\"controller_blacklist\"\\s*\")([^\"]*)(\"\\s*)$");
 
@@ -21,15 +26,6 @@ public class SteamPlatform : IPlatform
         { @"controller_base\desktop_neptune.vdf", Resources.empty_neptune },
         { @"controller_base\chord_neptune.vdf", Resources.chord_neptune },
         { @"controller_base\templates\controller_neptune_steamcontroller.vdf", Resources.empty_neptune },
-
-        // Desktop Configuration
-        { @"steamapps\common\Steam Controller Configs\42902745\config\413080\controller_neptune.vdf", Resources.empty_neptune },
-
-        // Steam Button Chord Basic Configuration
-        { @"steamapps\common\Steam Controller Configs\42902745\config\443510\controller_neptune.vdf", Resources.empty_neptune },
-
-        // { @"controller_base\bigpicture_neptune.vdf", Resources.chord_neptune },
-        // { @"controller_base\chord_neptune_external.vdf", Resources.chord_neptune },
     };
 
     public SteamPlatform()
@@ -38,6 +34,9 @@ public class SteamPlatform : IPlatform
 
         Name = "Steam";
         ExecutableName = "steam.exe";
+        
+        // this is for detecting steam start/stop, for some reason steam.exe often doesn't work
+        RunningName = "steamwebhelper.exe";
 
         // store specific modules
         Modules = new List<string>
@@ -60,6 +59,54 @@ public class SteamPlatform : IPlatform
 
             // check executable
             IsInstalled = File.Exists(ExecutablePath);
+        }
+    }
+
+    public void Start()
+    {
+        ProcessManager.ProcessStarted += ProcessManager_ProcessStarted;
+        ProcessManager.ProcessStopped += ProcessManager_ProcessStopped;
+    }
+
+    public void Stop()
+    {
+        ProcessManager.ProcessStarted -= ProcessManager_ProcessStarted;
+        ProcessManager.ProcessStopped -= ProcessManager_ProcessStopped;
+
+        // restore files even if Steam is still running
+        RestoreFiles();
+    }
+
+    private void ReplaceFiles()
+    {
+        // overwrite controller files
+        foreach (var config in ControllerFiles)
+            OverwriteFile(config.Key, config.Value, true);
+    }
+
+    private void RestoreFiles()
+    {
+        // restore controller files
+        foreach (var config in ControllerFiles)
+            ResetFile(config.Key);
+    }
+
+    private async void ProcessManager_ProcessStarted(ProcessEx processEx, bool OnStartup)
+    {
+        if (!OnStartup && processEx.Executable == RunningName)
+        {
+            LogManager.LogDebug("Steam started, replacing files in 3 seconds");
+            await Task.Delay(3000);
+            ReplaceFiles();
+        }
+    }
+
+    private void ProcessManager_ProcessStopped(ProcessEx processEx)
+    {
+        if (processEx.Executable == RunningName)
+        {
+            LogManager.LogDebug("Steam stopped, restoring files");
+            RestoreFiles();
         }
     }
 
