aa3f1236b89ee9a37feeab71b4cf14b646011ed7
xerootg <xerootg@users.noreply.github.com>
2023-11-13 11:16:00 -0700

    Aya Neo Air Plus 7320u config (#814)

---
+++ HandheldCompanion/Devices/AYANEO/AYANEOAIRPlusAMDMendocino.cs
@@ -0,0 +1,12 @@
+namespace HandheldCompanion.Devices;
+
+public class AYANEOAIRPlusAMDMendocino : AYANEOAIRPlus
+{
+    public AYANEOAIRPlusAMDMendocino()
+    {
+        // https://www.amd.com/en/products/apu/amd-ryzen-3-7320u
+        nTDP = new double[] { 12, 12, 12 };
+        cTDP = new double[] { 5, 15 };
+        GfxClock = new double[] { 100, 1900 };
+    }
+}
