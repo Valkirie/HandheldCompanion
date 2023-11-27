4233299783653894788ddf748c3c6458d1e9f254
Lesueur Benjamin <benjamin.lesueur@live.com>
2023-11-27 20:08:50 +0100

    fix Hint_LegionGoDaemon

--- HandheldCompanion/Controls/Hints/Hint_LegionGoDaemon.cs
+++ HandheldCompanion/Controls/Hints/Hint_LegionGoDaemon.cs
@@ -1,13 +1,13 @@
 ﻿using HandheldCompanion.Devices;
-using HandheldCompanion.Managers;
 using HandheldCompanion.Views;
 using Microsoft.Win32.TaskScheduler;
 using System;
 using System.Diagnostics;
 using System.Linq;
-using System.ServiceProcess;
+using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
+using Task = System.Threading.Tasks.Task;
 
 namespace HandheldCompanion.Controls.Hints
 {
@@ -15,14 +15,16 @@ namespace HandheldCompanion.Controls.Hints
     {
         private Process process;
         private const string taskName = "LSDaemon";
+        private Timer taskTimer;
 
         public Hint_LegionGoDaemon() : base()
         {
             if (MainWindow.CurrentDevice is not LegionGo)
                 return;
 
-            ProcessManager.ProcessStarted += ProcessManager_ProcessStarted;
-            ProcessManager.ProcessStopped += ProcessManager_ProcessStopped;
+            taskTimer = new Timer(4000);
+            taskTimer.Elapsed += TaskTimer_Elapsed;
+            taskTimer.Start();
 
             // default state
             this.HintActionButton.Visibility = Visibility.Visible;
@@ -34,35 +36,38 @@ namespace HandheldCompanion.Controls.Hints
             this.HintActionButton.Content = $"Disable {taskName}";
         }
 
-        private void ProcessManager_ProcessStarted(ProcessEx processEx, bool OnStartup)
+        private void TaskTimer_Elapsed(object? sender, ElapsedEventArgs e)
         {
-            if (processEx.Executable.Equals(taskName, StringComparison.InvariantCultureIgnoreCase))
-                this.Visibility = Visibility.Visible;
+            // Get all the processes with the given name
+            process = Process.GetProcessesByName(taskName).FirstOrDefault();
 
-            process = processEx.Process;
-        }
-
-        private void ProcessManager_ProcessStopped(ProcessEx processEx)
-        {
-            if (processEx.Executable.Equals(taskName, StringComparison.InvariantCultureIgnoreCase))
-                this.Visibility = Visibility.Collapsed;
-
-            process = null;
+            // If there is at least one process, return true
+            // UI thread (async)
+            Application.Current.Dispatcher.BeginInvoke(() =>
+            {
+                if (process is not null && !process.HasExited)
+                    this.Visibility = Visibility.Visible;
+                else
+                    this.Visibility = Visibility.Collapsed;
+            });
         }
 
         protected override void HintActionButton_Click(object sender, RoutedEventArgs e)
         {
-            // Get the task service instance
-            using (TaskService ts = new TaskService())
+            Task.Run(async () =>
             {
-                // Get the task by name
-                Task task = ts.GetTask(taskName);
-                if (task != null && task.State == TaskState.Running)
+                // Get the task service instance
+                using (TaskService ts = new TaskService())
                 {
-                    task.Stop();
-                    task.Enabled = false;
+                    // Get the task by name
+                    Microsoft.Win32.TaskScheduler.Task task = ts.GetTask(taskName);
+                    if (task != null && task.State == TaskState.Running)
+                    {
+                        task.Stop();
+                        task.Enabled = false;
+                    }
                 }
-            }
+            });
 
             // If the process exists and is running, kill it
             if (process != null && !process.HasExited)
