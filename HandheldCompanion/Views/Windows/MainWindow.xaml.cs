f0951c505fec92a48f24482b801c99e3556196ea
Lesueur Benjamin <benjamin.lesueur@live.com>
2024-01-12 11:32:56 +0100

    Implement CustomWpf (#154)
    * Disable the RealTimeStylus for WPF Applications
    
    * Update MainWindow.xaml.cs
    
    * test ?

--- HandheldCompanion/Views/Windows/MainWindow.xaml.cs
+++ HandheldCompanion/Views/Windows/MainWindow.xaml.cs
@@ -98,7 +98,32 @@ public partial class MainWindow : GamepadWindow
         var process = Process.GetCurrentProcess();
 
         // fix touch support
-        var tablets = Tablet.TabletDevices;
+        TabletDeviceCollection tabletDevices = Tablet.TabletDevices;
+        /*if (tabletDevices.Count > 0)
+        {
+            // Get the Type of InputManager.  
+            Type inputManagerType = typeof(System.Windows.Input.InputManager);
+
+            // Call the StylusLogic method on the InputManager.Current instance.  
+            object stylusLogic = inputManagerType.InvokeMember("StylusLogic",
+                        BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+                        null, InputManager.Current, null);
+
+            if (stylusLogic != null)
+            {
+                //  Get the type of the stylusLogic returned from the call to StylusLogic.  
+                Type stylusLogicType = stylusLogic.GetType();
+
+                // Loop until there are no more devices to remove.  
+                while (tabletDevices.Count > 0)
+                {
+                    // Remove the first tablet device in the devices collection.  
+                    stylusLogicType.InvokeMember("OnTabletRemoved",
+                            BindingFlags.InvokeMethod | BindingFlags.Instance | BindingFlags.NonPublic,
+                            null, stylusLogic, new object[] { (uint)0 });
+                }
+            }
+        }*/
 
         // get first start
         bool FirstStart = SettingsManager.GetBoolean("FirstStart");
