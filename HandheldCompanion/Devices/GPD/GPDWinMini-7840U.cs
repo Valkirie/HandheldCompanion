680ff02a5518be337cb4461b785c8cf052ca539a
CasperH2O <CasperH2O@users.noreply.github.com>
2024-01-24 15:33:55 +0000

    Support GPD WIN MINI (Initial) (#169)

---
+++ HandheldCompanion/Devices/GPD/GPDWinMini-7840U.cs
@@ -0,0 +1,83 @@
+﻿using HandheldCompanion.Inputs;
+using System.Collections.Generic;
+using System.Numerics;
+using WindowsInput.Events;
+
+namespace HandheldCompanion.Devices;
+
+public class GPDWinMini_7840U : IDevice
+{
+    public GPDWinMini_7840U()
+    {
+        // https://www.amd.com/en/products/apu/amd-ryzen-7-7840u
+        nTDP = new double[] { 15, 15, 18 };
+        cTDP = new double[] { 5, 18 };
+        GfxClock = new double[] { 200, 2700 };
+        CpuClock = 5100;
+
+        // device specific settings
+        ProductIllustration = "device_gpd_winmini";
+
+        // device specific capacities
+        //Capabilities = DeviceCapabilities.FanControl;
+
+        ECDetails = new ECDetails
+        {
+            AddressFanControl = 0x275,
+            AddressFanDuty = 0x1809,
+            AddressStatusCommandPort = 0x4E,
+            AddressDataPort = 0x4F,
+            FanValueMin = 0,
+            FanValueMax = 184
+        };
+
+        GyrometerAxis = new Vector3(1.0f, 1.0f, -1.0f);
+        GyrometerAxisSwap = new SortedDictionary<char, char>
+        {
+            { 'X', 'Y' },
+            { 'Y', 'Z' },
+            { 'Z', 'X' }
+        };
+
+        AccelerometerAxis = new Vector3(1.0f, -1.0f, 1.0f);
+        AccelerometerAxisSwap = new SortedDictionary<char, char>
+        {
+            { 'X', 'X' },
+            { 'Y', 'Z' },
+            { 'Z', 'Y' }
+        };
+
+        // Disabled this one as Win Max 2 also sends an Xbox guide input when Menu key is pressed.
+        OEMChords.Add(new DeviceChord("Menu",
+            new List<KeyCode> { KeyCode.LButton | KeyCode.XButton2 },
+            new List<KeyCode> { KeyCode.LButton | KeyCode.XButton2 },
+            true, ButtonFlags.OEM1
+        ));
+
+        // note, need to manually configured in GPD app
+        OEMChords.Add(new DeviceChord("L4",
+            new List<KeyCode> { KeyCode.F11, KeyCode.L },
+            new List<KeyCode> { KeyCode.F11, KeyCode.L },
+            false, ButtonFlags.OEM2
+        ));
+
+        OEMChords.Add(new DeviceChord("R4",
+            new List<KeyCode> { KeyCode.F12, KeyCode.R },
+            new List<KeyCode> { KeyCode.F12, KeyCode.R },
+            false, ButtonFlags.OEM3
+        ));
+    }
+
+    public override string GetGlyph(ButtonFlags button)
+    {
+        switch (button)
+        {
+            case ButtonFlags.OEM2:
+                return "\u2276";
+            case ButtonFlags.OEM3:
+                return "\u2277";
+        }
+
+        return defaultGlyph;
+    }
+}
--- HandheldCompanion/Devices/IDevice.cs
+++ HandheldCompanion/Devices/IDevice.cs
@@ -309,6 +309,17 @@ public abstract class IDevice
                         case "WIN2":
                             device = new GPDWin2();
                             break;
+                        case "G1617-01":
+                            switch (Processor)
+                            {
+                                case "AMD Ryzen 5 7640U w/ Radeon 760M Graphics":
+                                    device = new GPDWinMini_7640U();
+                                    break;
+                                case "AMD Ryzen 7 7840U w/ Radeon 780M Graphics":
+                                    device = new GPDWinMini_7840U();
+                                    break;
+                            }
+                            break;
                         case "G1618-03":
                             device = new GPDWin3();
                             break;
