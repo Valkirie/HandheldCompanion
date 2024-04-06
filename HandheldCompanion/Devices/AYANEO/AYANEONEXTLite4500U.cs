49bdb032b5d8dc1b622917d005d645784bd70e5b
Lesueur Benjamin <benjamin.lesueur@live.com>
2024-04-06 15:40:20 +0200

    fix compile error

--- HandheldCompanion/Devices/AYANEO/AYANEONEXTLite4500U.cs
+++ HandheldCompanion/Devices/AYANEO/AYANEONEXTLite4500U.cs
@@ -2,10 +2,10 @@
 
 public class AYANEONEXTLite4500U : AYANEONEXTLite
 {
-    public AYANEONEXTLite()
+    public AYANEONEXTLite4500U()
     {
         // https://www.amd.com/en/support/apu/amd-ryzen-processors/amd-ryzen-5-mobile-processors-radeon-graphics/amd-ryzen-5-4500u
         this.GfxClock = new double[] { 100, 1500 };
-        this.CpuClock = 4000 ;
+        this.CpuClock = 4000;
     }
 }
