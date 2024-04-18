894c9a085923ee8b2fbc0e697063e4e5d10f9a96
Lesueur Benjamin <benjamin.lesueur@live.com>
2024-04-18 11:07:29 +0200

    Fixed an issue causing a crash on device with no available audio output

--- HandheldCompanion/Views/Pages/SettingsPage.xaml.cs
+++ HandheldCompanion/Views/Pages/SettingsPage.xaml.cs
@@ -159,6 +159,7 @@ public partial class SettingsPage : Page
                     Toggle_QuicktoolsAutoHide.IsOn = Convert.ToBoolean(value);
                     break;
                 case "UISounds":
+                    Toggle_UISounds.IsEnabled = MultimediaManager.HasVolumeSupport();
                     Toggle_UISounds.IsOn = Convert.ToBoolean(value);
                     break;
             }
