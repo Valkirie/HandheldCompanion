634aeee267be89fcaf19af7d3d0cf7c208aaa12f
CasperH <casperhofstede@gmail.com>
2024-05-09 17:03:26 +0200

    AOKZOE A2 Support

---
+++ HandheldCompanion/Devices/AOKZOE/AOKZOEA2.cs
@@ -0,0 +1,10 @@
+﻿namespace HandheldCompanion.Devices;
+
+public class AOKZOEA2 : AOKZOEA1Pro
+{
+    public AOKZOEA2()
+    {
+        // device specific settings
+        ProductModel = "AOKZOEA2";
+    }
+}
