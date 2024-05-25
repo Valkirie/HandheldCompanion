8e822cd30e2cf8f04c93762a51b67fc065f91aec
Joshua Tam <297250+joshuatam@users.noreply.github.com>
2024-05-25 16:50:06 +0800

    OneXPlayer X1 Support

---
+++ HandheldCompanion/Devices/OneXPlayer/OneXPlayerX1AMD.cs
@@ -0,0 +1,22 @@
+﻿using HandheldCompanion.Inputs;
+using HandheldCompanion.Managers;
+using HandheldCompanion.Misc;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using WindowsInput.Events;
+using static HandheldCompanion.Utils.DeviceUtils;
+
+namespace HandheldCompanion.Devices;
+
+public class OneXPlayerX1AMD : OneXPlayerX1
+{
+    public OneXPlayerX1AMD()
+    {
+        // https://www.amd.com/en/products/apu/amd-ryzen-7-8840u
+        nTDP = new double[] { 15, 15, 28 };
+        cTDP = new double[] { 15, 30 };
+        GfxClock = new double[] { 100, 2700 };
+        CpuClock = 5100;
+    }
+}
