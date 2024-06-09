d15691fcbf7cabecf34710d9379f5dd12e5410c0
JT <297250+joshuatam@users.noreply.github.com>
2024-06-09 21:12:21 +0800

    Add 3 Power Profiles for X1 Intel (#297)
    * Add 3 Profiles for X1 Intel
    
    * update X1 super performance with EPP override

--- HandheldCompanion/Devices/OneXPlayer/OneXPlayerX1Intel.cs
+++ HandheldCompanion/Devices/OneXPlayer/OneXPlayerX1Intel.cs
@@ -1,4 +1,6 @@
-﻿namespace HandheldCompanion.Devices;
+﻿using HandheldCompanion.Managers;
+
+namespace HandheldCompanion.Devices;
 
 public class OneXPlayerX1Intel : OneXPlayerX1
 {
@@ -10,5 +12,43 @@ public class OneXPlayerX1Intel : OneXPlayerX1
         cTDP = new double[] { 6, 35 };
         GfxClock = new double[] { 100, 2250 };
         CpuClock = 4800;
+        
+        // Power Saving
+        DevicePowerProfiles.Add(new(Properties.Resources.PowerProfileOneXPlayerX1IntelBetterBattery, Properties.Resources.PowerProfileOneXPlayerX1IntelBetterBatteryDesc)
+        {
+            Default = true,
+            DeviceDefault = true,
+            OSPowerMode = OSPowerMode.BetterBattery,
+            CPUBoostLevel = CPUBoostLevel.Disabled,
+            Guid = new("961cc777-2547-4f9d-8174-7d86181b8a7a"),
+            TDPOverrideEnabled = true,
+            TDPOverrideValues = new[] { 15.0d, 15.0d, 15.0d }
+        });
+
+        // Performance
+        DevicePowerProfiles.Add(new(Properties.Resources.PowerProfileOneXPlayerX1IntelBetterPerformance, Properties.Resources.PowerProfileOneXPlayerX1IntelBetterPerformanceDesc)
+        {
+            Default = true,
+            DeviceDefault = true,
+            OSPowerMode = OSPowerMode.BetterPerformance,
+            CPUBoostLevel = CPUBoostLevel.Enabled,
+            Guid = new("3af9B8d9-7c97-431d-ad78-34a8bfea439f"),
+            TDPOverrideEnabled = true,
+            TDPOverrideValues = new[] { 30.0d, 30.0d, 30.0d }
+        });
+        
+        // Max Performance
+        DevicePowerProfiles.Add(new(Properties.Resources.PowerProfileOneXPlayerX1IntelBestPerformance, Properties.Resources.PowerProfileOneXPlayerX1IntelBestPerformanceDesc)
+        {
+            Default = true,
+            DeviceDefault = true,
+            OSPowerMode = OSPowerMode.BestPerformance,
+            CPUBoostLevel = CPUBoostLevel.Enabled,
+            Guid = new("ded574b5-45a0-4f42-8737-46345c09c238"),
+            TDPOverrideEnabled = true,
+            TDPOverrideValues = new[] { 35.0d, 35.0d, 64.0d },
+            EPPOverrideEnabled = true,
+            EPPOverrideValue = 32,
+        });
     }
 }
