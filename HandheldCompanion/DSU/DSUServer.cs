f3ed79138f6a25c4d6391788de86a89750b87f47
Lesueur Benjamin <benjamin.lesueur@live.com>
2024-07-09 16:16:15 +0200

    fixed an issue causing a crash on disposed DSUServer udpSock

--- HandheldCompanion/DSU/DSUServer.cs
+++ HandheldCompanion/DSU/DSUServer.cs
@@ -419,10 +419,18 @@ public class DSUServer
     /// </summary>
     private void ResetUDPConn()
     {
+        if (udpSock is null)
+            return;
+
         uint IOC_IN = 0x80000000;
         uint IOC_VENDOR = 0x18000000;
         uint SIO_UDP_CONNRESET = IOC_IN | IOC_VENDOR | 12;
-        udpSock?.IOControl((int)SIO_UDP_CONNRESET, new byte[] { Convert.ToByte(false) }, null);
+
+        try
+        {
+            udpSock.IOControl((int)SIO_UDP_CONNRESET, new byte[] { Convert.ToByte(false) }, null);
+        }
+        catch (ObjectDisposedException) { }
     }
 
     public bool Start()
