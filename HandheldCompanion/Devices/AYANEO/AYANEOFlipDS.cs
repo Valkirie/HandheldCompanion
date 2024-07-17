606bd61483129697fbf6026b96067605df7766c5
Lesueur Benjamin <benjamin.lesueur@live.com>
2024-07-17 17:01:44 +0200

    AYANEO Flip DS: Implement Custom Key Screen button

--- HandheldCompanion/Devices/AYANEO/AYANEOFlipDS.cs
+++ HandheldCompanion/Devices/AYANEO/AYANEOFlipDS.cs
@@ -1,3 +1,6 @@
+using HandheldCompanion.Inputs;
+using WindowsInput.Events;
+
 namespace HandheldCompanion.Devices;
 
 public class AYANEOFlipDS : AYANEOFlipKB
@@ -13,6 +16,22 @@ public class AYANEOFlipDS : AYANEOFlipKB
         this.Capabilities -= DeviceCapabilities.DynamicLightingBrightness;
 
         // TODO: Add OEMChords for "Dual-Screen Keys" key here
+        this.OEMChords.Add(new KeyboardChord("Custom Key Screen",
+            [KeyCode.RControlKey, KeyCode.LWin, KeyCode.F18],
+            [KeyCode.F18, KeyCode.LWin, KeyCode.RControlKey],
+            false, ButtonFlags.OEM5
+        ));
+    }
+
+    public override string GetGlyph(ButtonFlags button)
+    {
+        switch (button)
+        {
+            case ButtonFlags.OEM5:
+                return "\u23CD";
+        }
+
+        return base.GetGlyph(button);
     }
 
     protected void CEcControl_SetSecDispBrightness(short brightness)
