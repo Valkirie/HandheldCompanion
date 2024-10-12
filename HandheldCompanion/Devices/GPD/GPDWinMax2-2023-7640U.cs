19a826e061805e54a9dbd37c4c6f4d2e52b3fade
Lesueur Benjamin <benjamin.lesueur@live.com>
2024-10-12 18:29:54 +0200

    oups

--- HandheldCompanion/Devices/GPD/GPDWinMax2-2023-7640U.cs
+++ HandheldCompanion/Devices/GPD/GPDWinMax2-2023-7640U.cs
@@ -8,7 +8,7 @@ public class GPDWinMax2_2023_7640U : GPDWinMax2
     public GPDWinMax2_2023_7640U()
     {
         // https://www.amd.com/en/products/processors/laptop/ryzen/7000-series/amd-ryzen-5-7640u.html
-        nTDP = new double[] { 15, 3, 28 }; //default, low, high
+        nTDP = new double[] { 15, 15, 28 };
         cTDP = new double[] { 3, 28 };
         GfxClock = new double[] { 100, 2600 };
         CpuClock = 4900;
