85ffe25674a8b2e7b1603577745495f4e4516fe9
Lesueur Benjamin <benjamin.lesueur@live.com>
2025-02-12 18:14:02 +0100

    fix KeyboardActions

--- HandheldCompanion/Actions/KeyboardActions.cs
+++ HandheldCompanion/Actions/KeyboardActions.cs
@@ -18,13 +18,12 @@ namespace HandheldCompanion.Actions
         // settings
         public ModifierSet Modifiers = ModifierSet.None;
 
-        // override Value
-        public new bool Value = false;
-        public new bool prevValue = false;
-
         public KeyboardActions()
         {
             this.actionType = ActionType.Keyboard;
+
+            this.Value = false;
+            this.prevValue = false;
         }
 
         public KeyboardActions(VirtualKeyCode key) : this()
