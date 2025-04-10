9c03739ff26e4d7bbb5a574d289552a950543c39
Sanheiii <35133371+Sanheiii@users.noreply.github.com>
2025-04-10 17:13:17 +0800

    feat(Localization): use system language when user language is not set

---
+++ HandheldCompanion/Converters/CultureToDisplayNameConverter.cs
@@ -0,0 +1,29 @@
+﻿using HandheldCompanion.Localization;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HandheldCompanion.Converters
+{
+    class CultureToDisplayNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is CultureInfo sourceCulture)
+            {
+                return sourceCulture.NativeName;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
