a88e75caf587d9651ff624f936041aae0be4d2d6
Sanheiii <35133371+Sanheiii@users.noreply.github.com>
2025-04-22 11:38:42 +0800

    feat(i18n): Inject custom ResourceManager with en-US fallback

---
+++ HandheldCompanion/Localization/ResilientResourceManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandheldCompanion.Localization
+{
+    public class ResilientResourceManager : ResourceManager
+    {
+        private static readonly CultureInfo fallbackCulture = new CultureInfo("en-US");
+
+        public ResilientResourceManager(string baseName, System.Reflection.Assembly assembly) : base(baseName, assembly)
+        {
+        }
+
+        public override string GetString(string name, CultureInfo? culture)
+        {
+            string? result = base.GetString(name, culture);
+            if (string.IsNullOrEmpty(result))
+                result = base.GetString(name, fallbackCulture);
+            return result ?? $"[{name}]";
+        }
+
+        public override string GetString(string name)
+        {
+            return GetString(name, CultureInfo.CurrentUICulture);
+        }
+    }
+}
