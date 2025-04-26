822a826b88e8b2f4fce3643fcdd83213a53839ed
Lesueur Benjamin <benjamin.lesueur@live.com>
2025-04-26 15:56:51 +0200

    Set LayoutMode default value to Gamepad

--- HandheldCompanion/Properties/Settings.Designer.cs
+++ HandheldCompanion/Properties/Settings.Designer.cs
@@ -1228,7 +1228,7 @@ namespace HandheldCompanion.Properties
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
         public int LayoutMode {
             get {
                 return ((int)(this["LayoutMode"]));
