ce3814b4f7850e15e6b37346497bcda13e65263f
Lesueur Benjamin <benjamin.lesueur@live.com>
2025-05-02 18:38:30 +0200

    Tentative: XClawController

---
+++ HandheldCompanion/Controllers/XClawController.cs
@@ -0,0 +1,26 @@
+﻿using HandheldCompanion.Devices;
+
+namespace HandheldCompanion.Controllers
+{
+    public class XClawController : XInputController
+    {
+        public override bool IsReady
+        {
+            get
+            {
+                if (IDevice.GetCurrent() is ClawA1M clawA1M)
+                    return clawA1M.IsOpen;
+
+                return false;
+            }
+        }
+
+        public XClawController()
+        { }
+
+        public XClawController(PnPDetails details) : base()
+        {
+            AttachDetails(details);
+        }
+    }
+}
