7e02969b70fa89a8e987a9f9ddb5494e03c3d5ce
Lesueur Benjamin <benjamin.lesueur@live.com>
2025-05-21 14:59:26 +0200

    Fix MSI Claw 8AI+ default power profiles TDP values

--- HandheldCompanion/Devices/MSI/ClawA2VM.cs
+++ HandheldCompanion/Devices/MSI/ClawA2VM.cs
@@ -14,8 +14,8 @@ public class ClawA2VM : ClawA1M
         ProductIllustration = "device_msi_claw8";
 
         // https://www.intel.com/content/www/us/en/products/sku/240957/intel-core-ultra-7-processor-258v-12m-cache-up-to-4-80-ghz/specifications.html
-        nTDP = new double[] { 28, 28, 37 };
-        cTDP = new double[] { 20, 37 };
+        nTDP = new double[] { 17, 17, 37 };
+        cTDP = new double[] { 8, 30 };
         GfxClock = new double[] { 100, 1950 };
         CpuClock = 4800;
 
@@ -24,9 +24,9 @@ public class ClawA2VM : ClawA1M
         // overwrite ClawA1M default power profiles
         Dictionary<Guid, double[]> tdpOverrides = new Dictionary<Guid, double[]>
         {
-            { BetterBatteryGuid,      new double[] { 20.0d, 20.0d, 20.0d } },
-            { BetterPerformanceGuid,  new double[] { 30.0d, 30.0d, 30.0d } },
-            { BestPerformanceGuid,    new double[] { 35.0d, 35.0d, 35.0d } }
+            { BetterBatteryGuid,      new double[] { 8.0d, 8.0d, 8.0d } },
+            { BetterPerformanceGuid,  new double[] { 17.0d, 17.0d, 17.0d } },
+            { BestPerformanceGuid,    new double[] { 30.0d, 30.0d, 30.0d } }
         };
 
         foreach (KeyValuePair<Guid, double[]> kvp in tdpOverrides)
