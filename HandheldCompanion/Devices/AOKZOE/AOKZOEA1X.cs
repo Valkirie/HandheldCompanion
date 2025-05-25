feed96021c3bc56be105c18348689bbfee9d4a86
Lesueur Benjamin <benjamin.lesueur@live.com>
2025-05-25 09:21:58 +0200

    (WIP) AOKZOEA1X

---
+++ HandheldCompanion/Devices/AOKZOE/AOKZOEA1X.cs
@@ -0,0 +1,14 @@
+﻿namespace HandheldCompanion.Devices;
+
+public class AOKZOEA1X : AOKZOEA1Pro
+{
+    public AOKZOEA1X()
+    {
+        // device specific settings
+        ProductModel = "AOKZOEA1X";
+
+        // https://www.amd.com/en/products/processors/laptop/ryzen/300-series/amd-ryzen-ai-9-365.html
+        GfxClock = new double[] { 100, 2900 };
+        CpuClock = 5100;
+    }
+}
