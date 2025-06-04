b087719b5e3e4b32f9e3b683424e0365872ebd42
Lesueur Benjamin <benjamin.lesueur@live.com>
2025-06-04 17:43:04 +0200

    Update SecretKeys.cs

--- HandheldCompanion/SecretKeys.cs
+++ HandheldCompanion/SecretKeys.cs
@@ -2,8 +2,9 @@ namespace HandheldCompanion;
 
 public static class SecretKeys
 {
-    public static string DSN_URL = "https://c7af4f166cafa539ed02c3ced8ad76db@o4507394726232064.ingest.de.sentry.io/4507394776694864";
-    public static string IGDB_CLIENT_ID = "3o9vra2ls70yqyvexwayqdle5zkwdb";
-    public static string IGDB_CLIENT_SECRET = "oev5zqdtfr77r24lgv21pt07cocr88";
-    public static string STEAMGRID_CLIENT_SECRET = "2336f10f228aa5261559a38bbd483a07";
+    // Register at Sentry.io and paste the DSN url here
+    public static string DSN_URL = "";
+    public static string IGDB_CLIENT_ID = "";
+    public static string IGDB_CLIENT_SECRET = "";
+    public static string STEAMGRID_CLIENT_SECRET = "";
 }
