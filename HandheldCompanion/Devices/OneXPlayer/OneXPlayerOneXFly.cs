5af548f43a01840db865b3b042fdc998397840e2
Lesueur Benjamin <benjamin.lesueur@live.com>
2025-06-20 11:35:03 +0200

    misc

--- HandheldCompanion/Devices/OneXPlayer/OneXPlayerOneXFly.cs
+++ HandheldCompanion/Devices/OneXPlayer/OneXPlayerOneXFly.cs
@@ -19,6 +19,7 @@ public class OneXPlayerOneXFly : IDevice
         // device specific settings
         ProductIllustration = "device_onexplayer_onexfly";
         ProductModel = "ONEXPLAYEROneXFly";
+        UseOpenLib = true;
 
         // https://www.amd.com/en/products/apu/amd-ryzen-z1
         // https://www.amd.com/en/products/apu/amd-ryzen-z1-extreme
