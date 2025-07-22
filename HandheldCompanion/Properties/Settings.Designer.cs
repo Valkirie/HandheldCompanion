8ff717a87a3346bb2e31b77a83270a022f50fc1d
Lesueur Benjamin <benjamin.lesueur@live.com>
2025-07-22 18:01:23 +0200

    Fix settings

--- HandheldCompanion/Properties/Settings.Designer.cs
+++ HandheldCompanion/Properties/Settings.Designer.cs
@@ -7,22 +7,24 @@
 //     kod zostanie ponownie wygenerowany.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System.Configuration;
 
-namespace HandheldCompanion.Properties {
-    
-    
-    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+namespace HandheldCompanion.Properties
+{
+
+    [SettingsProvider(typeof(CustomSettingsProvider))]
+    internal sealed partial class Settings : ApplicationSettingsBase
+    {
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
             }
         }
-        
+
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
