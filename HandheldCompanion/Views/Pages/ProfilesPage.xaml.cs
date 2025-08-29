0fa451224bd2020d85d117078e19298ef814fe1d
Lesueur Benjamin <benjamin.lesueur@live.com>
2025-08-29 15:37:34 +0200

    Fixed: Profile update when resolution or refresh rate is changed

--- HandheldCompanion/Views/Pages/ProfilesPage.xaml.cs
+++ HandheldCompanion/Views/Pages/ProfilesPage.xaml.cs
@@ -266,17 +266,28 @@ public partial class ProfilesPage : Page
     {
         List<ScreenFramelimit> frameLimits = desktopScreen.GetFramelimits();
 
-        // UI thread
-        UIHelper.TryInvoke(() =>
+        if (profileLock.TryEnter())
         {
-            cB_Framerate.Items.Clear();
+            try
+            {
+                // UI thread
+                UIHelper.TryInvoke(() =>
+                {
+                    cB_Framerate.Items.Clear();
 
-            foreach (ScreenFramelimit frameLimit in frameLimits)
-                cB_Framerate.Items.Add(frameLimit);
+                    foreach (ScreenFramelimit frameLimit in frameLimits)
+                        cB_Framerate.Items.Add(frameLimit);
 
-            if (selectedProfile is not null)
-                cB_Framerate.SelectedItem = desktopScreen.GetClosest(selectedProfile.FramerateValue);
-        });
+                    if (selectedProfile is not null)
+                        cB_Framerate.SelectedItem = desktopScreen.GetClosest(selectedProfile.FramerateValue);
+                });
+            }
+            catch { }
+            finally
+            {
+                profileLock.Exit();
+            }
+        }
     }
 
     private void Page_Loaded(object sender, RoutedEventArgs e)
