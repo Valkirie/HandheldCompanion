1b1fdc85e07813932436daf39efee872a359c964
Chenx Dust <chenx_dust@outlook.com>
2025-10-02 22:16:01 +0800

    Add WinUI 2 and Xbox Game Bar dependencies

--- HandheldCompanion.UWP/App.xaml.cs
+++ HandheldCompanion.UWP/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Gaming.XboxGameBar;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,6 +8,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System.Profile;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -18,13 +20,15 @@ using Windows.UI.Xaml.Navigation;
 namespace HandheldCompanion.UWP
 {
     /// <summary>
-    /// 提供特定于应用程序的行为，以补充默认的应用程序类。
+    /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
     sealed partial class App : Application
     {
+        private XboxGameBarWidget _xboxGameBarWidget;
+
         /// <summary>
-        /// 初始化单一实例应用程序对象。这是执行的创作代码的第一行，
-        /// 已执行，逻辑上等同于 main() 或 WinMain()。
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
         public App()
         {
@@ -32,30 +36,98 @@ namespace HandheldCompanion.UWP
             this.Suspending += OnSuspending;
         }
 
+
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            XboxGameBarWidgetActivatedEventArgs widgetArgs = null;
+            if (args.Kind == ActivationKind.Protocol)
+            {
+                var protocolArgs = args as IProtocolActivatedEventArgs;
+                string scheme = protocolArgs.Uri.Scheme;
+                if (scheme.Equals("ms-gamebarwidget"))
+                {
+                    widgetArgs = args as XboxGameBarWidgetActivatedEventArgs;
+                }
+            }
+            if (widgetArgs != null)
+            {
+                //
+                // Activation Notes:
+                //
+                //    If IsLaunchActivation is true, this is Game Bar launching a new instance
+                // of our widget. This means we have a NEW CoreWindow with corresponding UI
+                // dispatcher, and we MUST create and hold onto a new XboxGameBarWidget.
+                //
+                // Otherwise this is a subsequent activation coming from Game Bar. We MUST
+                // continue to hold the XboxGameBarWidget created during initial activation
+                // and ignore this repeat activation, or just observe the URI command here and act 
+                // accordingly.  It is ok to perform a navigate on the root frame to switch 
+                // views/pages if needed.  Game Bar lets us control the URI for sending widget to
+                // widget commands or receiving a command from another non-widget process. 
+                //
+                // Important Cleanup Notes:
+                //    When our widget is closed--by Game Bar or us calling XboxGameBarWidget.Close()-,
+                // the CoreWindow will get a closed event.  We can register for Window.Closed
+                // event to know when our particular widget has shutdown, and cleanup accordingly.
+                //
+                // NOTE: If a widget's CoreWindow is the LAST CoreWindow being closed for the process
+                // then we won't get the Window.Closed event.  However, we will get the OnSuspending
+                // call and can use that for cleanup.
+                //
+                if (widgetArgs.IsLaunchActivation)
+                {
+                    var rootFrame = new Frame();
+                    rootFrame.NavigationFailed += OnNavigationFailed;
+                    Window.Current.Content = rootFrame;
+
+                    // Create Game Bar widget object which bootstraps the connection with Game Bar
+                    _xboxGameBarWidget = new XboxGameBarWidget(
+                        widgetArgs,
+                        Window.Current.CoreWindow,
+                        rootFrame);
+                    rootFrame.Navigate(typeof(MainPage));
+
+                    Window.Current.Closed += XboxGameBarWidgetWindow_Closed;
+
+                    Window.Current.Activate();
+                }
+                else
+                {
+                    // You can perform whatever behavior you need based on the URI payload.
+                }
+            }
+        }
+
+        private void XboxGameBarWidgetWindow_Closed(object sender, Windows.UI.Core.CoreWindowEventArgs e)
+        {
+            _xboxGameBarWidget = null;
+            Window.Current.Closed -= XboxGameBarWidgetWindow_Closed;
+        }
+
         /// <summary>
-        /// 在应用程序由最终用户正常启动时进行调用。
-        /// 将在启动应用程序以打开特定文件等情况下使用。
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
         /// </summary>
-        /// <param name="e">有关启动请求和过程的详细信息。</param>
+        /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
             Frame rootFrame = Window.Current.Content as Frame;
 
-            // 不要在窗口已包含内容时重复应用程序初始化，
-            // 只需确保窗口处于活动状态
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
             if (rootFrame == null)
             {
-                // 创建要充当导航上下文的框架，并导航到第一页
+                // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
-                    //TODO: 从之前挂起的应用程序加载状态
+                    //TODO: Load state from previously suspended application
                 }
 
-                // 将框架放在当前窗口中
+                // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
 
@@ -63,38 +135,46 @@ namespace HandheldCompanion.UWP
             {
                 if (rootFrame.Content == null)
                 {
-                    // 当导航堆栈尚未还原时，导航到第一页，
-                    // 并通过将所需信息作为导航参数传入来配置
-                    // 参数
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
                     rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
-                // 确保当前窗口处于活动状态
+                // Ensure the current window is active
                 Window.Current.Activate();
             }
         }
 
         /// <summary>
-        /// 导航到特定页失败时调用
+        /// Invoked when Navigation to a certain page fails
         /// </summary>
-        ///<param name="sender">导航失败的框架</param>
-        ///<param name="e">有关导航失败的详细信息</param>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
         void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
 
         /// <summary>
-        /// 在将要挂起应用程序执行时调用。  在不知道应用程序
-        /// 无需知道应用程序会被终止还是会恢复，
-        /// 并让内存内容保持不变。
+        /// Invoked when application execution is being suspended.  Normally we
+        /// wouldn't know if the app was being terminated or just suspended at this
+        /// point. However, the app will never be suspended if Game Bar has an
+        /// active widget connection to it, so if you see this call it's safe to
+        /// cleanup any widget related objects. Keep in mind if all widgets are closed
+        /// and you have a foreground window for your app, this could still result in 
+        /// suspend or terminate. Regardless, it should always be safe to cleanup
+        /// your widget related objects.
         /// </summary>
-        /// <param name="sender">挂起的请求的源。</param>
-        /// <param name="e">有关挂起请求的详细信息。</param>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: 保存应用程序状态并停止任何后台活动
+
+            _xboxGameBarWidget = null;
+
             deferral.Complete();
         }
     }
 }
+
--- HandheldCompanion.UWP/MainPage.xaml.cs
+++ HandheldCompanion.UWP/MainPage.xaml.cs
@@ -13,13 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
-
 namespace HandheldCompanion.UWP
 {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
