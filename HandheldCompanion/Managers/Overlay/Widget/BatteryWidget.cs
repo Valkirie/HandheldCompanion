afb089d54ae6d6a19de644395c1172fbbfd8ebb8
mops1k <bednyj.mops@gmail.com>
2025-10-11 13:57:48 +0300

    - change battery left to hours and minutes format

--- HandheldCompanion/Managers/Overlay/Widget/BatteryWidget.cs
+++ HandheldCompanion/Managers/Overlay/Widget/BatteryWidget.cs
@@ -1,21 +1,79 @@
+using System.Management;
+
 namespace HandheldCompanion.Managers.Overlay.Widget;
 
 public class BatteryWidget: IWidget
 {
+    private float? TimeLeftInMinutes => PlatformManager.LibreHardwareMonitor.GetBatteryTimeSpan();
+
     public void Build(OverlayEntry entry, short? level = null)
     {
-        var _level = level ?? OSDManager.OverlayBATTLevel;
-        switch (_level)
+        var overlayBattLevel = level ?? OSDManager.OverlayBATTLevel;
+        switch (overlayBattLevel)
         {
             case WidgetLevel.FULL:
                 OSDManager.AddElementIfNotNull(entry, PlatformManager.LibreHardwareMonitor.GetBatteryLevel(), "%");
                 OSDManager.AddElementIfNotNull(entry, PlatformManager.LibreHardwareMonitor.GetBatteryPower(), "W");
-                OSDManager.AddElementIfNotNull(entry, PlatformManager.LibreHardwareMonitor.GetBatteryTimeSpan(), "min");
+                if (!IsBatteryCharging() && null != TimeLeftInMinutes)
+                {
+                    OSDManager.AddElementIfNotNull(entry, TimeBatteryHours(), "h");
+                    OSDManager.AddElementIfNotNull(entry, TimeBatteryMinutes(), "m");
+                }
+
                 break;
             case WidgetLevel.MINIMAL:
                 OSDManager.AddElementIfNotNull(entry, PlatformManager.LibreHardwareMonitor.GetBatteryLevel(), "%");
-                OSDManager.AddElementIfNotNull(entry, PlatformManager.LibreHardwareMonitor.GetBatteryTimeSpan(), "min");
+                if (!IsBatteryCharging() && null != TimeLeftInMinutes)
+                {
+                    OSDManager.AddElementIfNotNull(entry, TimeBatteryHours(), "h");
+                    OSDManager.AddElementIfNotNull(entry, TimeBatteryMinutes(), "m");
+                }
                 break;
         }
+    }
+
+    private static bool IsBatteryCharging()
+    {
+        var wmi = new ManagementObjectSearcher("SELECT * FROM Win32_Battery");
+        var resultCollection = wmi.Get();
+        foreach (var resultRow in resultCollection)
+        {
+            foreach (var property in resultRow.Properties)
+            {
+                if (property.Name != "BatteryStatus") continue;
+
+                var value = property.Value.ToString();
+                if (null == value)
+                {
+                    return false;
+                }
+
+                var status= ushort.Parse(value);
+
+                return (status & 2) == 2 || (status & 3) == 3;
+            }
+        }
+
+        return false;
+    }
+
+    private int TimeBatteryHours()
+    {
+        if (TimeLeftInMinutes != null)
+        {
+            return (int) (TimeLeftInMinutes / 60);
+        }
+
+        return 0;
+    }
+
+    private int TimeBatteryMinutes()
+    {
+        if (TimeLeftInMinutes != null)
+        {
+            return (int) (TimeLeftInMinutes - TimeLeftInMinutes / 60 * 60);
+        }
+
+        return 0;
     }
 }
