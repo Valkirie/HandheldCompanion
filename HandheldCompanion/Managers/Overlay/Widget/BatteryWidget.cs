283187bc72715534b0a1fdc923d2d76c8a57a150
Kvintilyanov Aleksandr <mops1k@users.noreply.github.com>
2025-10-18 17:36:19 +0300

    Change battery overlay widget to better logic and display (#1306)
    * Refactor Overlay rendering logic to strategy design pattern
    
    * Fix ai comments
    
    * Add widget system with factory
    
    * Fix some AI comments
    
    * Add level selection for overlay widgets.
    Refactor strategies (use widgets.
    
    * Fix level for cpu widget in full overlay strategy
    
    * - change battery left to hours and minutes format
    
    * - add format for hours
    
    * - fix after merge from `main`
    
    * - little refactoring
    
    * - add hours format
    
    * - add return if null time left
    
    * good to go
    
    ---------
    
    Co-authored-by: Lesueur Benjamin <benjamin.lesueur@live.com>

--- HandheldCompanion/Managers/Overlay/Widget/BatteryWidget.cs
+++ HandheldCompanion/Managers/Overlay/Widget/BatteryWidget.cs
@@ -1,21 +1,53 @@
+using System.Management;
+using System.Windows.Forms;
+
 namespace HandheldCompanion.Managers.Overlay.Widget;
 
 public class BatteryWidget : IWidget
 {
+    private float? TimeLeftInMinutes => PlatformManager.LibreHardware.GetBatteryTimeSpan();
+
     public void Build(OverlayEntry entry, short? level = null)
     {
-        var _level = level ?? OSDManager.OverlayBATTLevel;
+        short _level = level ?? OSDManager.OverlayBATTLevel;
         switch (_level)
         {
             case WidgetLevel.FULL:
                 OSDManager.AddElementIfNotNull(entry, PlatformManager.LibreHardware.GetBatteryLevel(), "%");
                 OSDManager.AddElementIfNotNull(entry, PlatformManager.LibreHardware.GetBatteryPower(), "W");
-                OSDManager.AddElementIfNotNull(entry, PlatformManager.LibreHardware.GetBatteryTimeSpan(), "min");
                 break;
             case WidgetLevel.MINIMAL:
                 OSDManager.AddElementIfNotNull(entry, PlatformManager.LibreHardware.GetBatteryLevel(), "%");
-                OSDManager.AddElementIfNotNull(entry, PlatformManager.LibreHardware.GetBatteryTimeSpan(), "min");
                 break;
+            default:
+                return;
         }
+
+        if (IsBatteryCharging())
+            return;
+
+        if (!TimeLeftInMinutes.HasValue)
+            return;
+
+        OSDManager.AddElementIfNotNull(entry, TimeBatteryHours(), "h");
+        OSDManager.AddElementIfNotNull(entry, TimeBatteryMinutes(), "min");
+    }
+
+    private static bool IsBatteryCharging() => SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Online;
+
+    private int TimeBatteryHours()
+    {
+        if (TimeLeftInMinutes is not float minutes)
+            return 0;
+
+        return (int)(minutes / 60f);
+    }
+
+    private int TimeBatteryMinutes()
+    {
+        if (TimeLeftInMinutes is not float minutes)
+            return 0;
+
+        return (int)(minutes % 60f);
     }
 }
