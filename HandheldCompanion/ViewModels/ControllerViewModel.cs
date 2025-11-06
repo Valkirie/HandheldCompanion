50583b43ae5626a69af1761c16fcbc608b528e02
Lesueur Benjamin <benjamin.lesueur@live.com>
2025-11-06 15:11:17 +0100

    Fixed: Rare crash on null Controller

--- HandheldCompanion/ViewModels/ControllerViewModel.cs
+++ HandheldCompanion/ViewModels/ControllerViewModel.cs
@@ -114,15 +114,15 @@ namespace HandheldCompanion.ViewModels
                 await Task.Run(() =>
                 {
                     if (IsHidden)
-                        Controller.Unhide();
+                        Controller?.Unhide();
                     else
-                        Controller.Hide();
+                        Controller?.Hide();
                 });
             });
 
             CalibrateCommand = new DelegateCommand(async () =>
             {
-                Controller.Calibrate();
+                Controller?.Calibrate();
             });
 
             SwitchLayoutCommand = new DelegateCommand(async () =>
