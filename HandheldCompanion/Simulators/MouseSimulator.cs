a83d05e6c1c43d01e6b3272fe3090305a845e099
pepordev <parraortega.pedro@gmail.com>
2025-12-16 08:55:18 +0100

    fix: make the toggle state shared and match the real status (#1343)
    Signed-off-by: Pedro Parra Ortega <pedro.parraortega@enreach.com>

--- HandheldCompanion/Simulators/MouseSimulator.cs
+++ HandheldCompanion/Simulators/MouseSimulator.cs
@@ -1,6 +1,7 @@
 ﻿using GregsStack.InputSimulatorStandard;
 using HandheldCompanion.Actions;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace HandheldCompanion.Simulators;
@@ -9,10 +10,26 @@ public static class MouseSimulator
 {
     private static readonly InputSimulator InputSimulator;
 
+    // Shared state tracking for mouse buttons - allows multiple actions to check the same state
+    private static readonly Dictionary<MouseActionsType, bool> ButtonStates = new();
+    private static readonly object StateLock = new();
+
     static MouseSimulator()
     {
         InputSimulator = new InputSimulator();
         InputSimulator.Mouse.MouseWheelClickSize = 6;
+    }
+
+    /// <summary>
+    /// Returns the shared state of a mouse button.
+    /// Used for toggle desync detection when multiple actions target the same button.
+    /// </summary>
+    public static bool IsButtonDown(MouseActionsType type)
+    {
+        lock (StateLock)
+        {
+            return ButtonStates.TryGetValue(type, out var state) && state;
+        }
     }
 
     public static void MouseDown(MouseActionsType type, int scrollAmountInClicks = 0)
@@ -23,12 +40,15 @@ public static class MouseSimulator
             {
                 case MouseActionsType.LeftButton:
                     InputSimulator.Mouse.LeftButtonDown();
+                    lock (StateLock) { ButtonStates[type] = true; }
                     break;
                 case MouseActionsType.RightButton:
                     InputSimulator.Mouse.RightButtonDown();
+                    lock (StateLock) { ButtonStates[type] = true; }
                     break;
                 case MouseActionsType.MiddleButton:
                     InputSimulator.Mouse.MiddleButtonDown();
+                    lock (StateLock) { ButtonStates[type] = true; }
                     break;
                 case MouseActionsType.ScrollUp:
                     InputSimulator.Mouse.VerticalScroll(scrollAmountInClicks);
@@ -52,12 +72,15 @@ public static class MouseSimulator
             {
                 case MouseActionsType.LeftButton:
                     InputSimulator.Mouse.LeftButtonUp();
+                    lock (StateLock) { ButtonStates[type] = false; }
                     break;
                 case MouseActionsType.RightButton:
                     InputSimulator.Mouse.RightButtonUp();
+                    lock (StateLock) { ButtonStates[type] = false; }
                     break;
                 case MouseActionsType.MiddleButton:
                     InputSimulator.Mouse.MiddleButtonUp();
+                    lock (StateLock) { ButtonStates[type] = false; }
                     break;
             }
         }
