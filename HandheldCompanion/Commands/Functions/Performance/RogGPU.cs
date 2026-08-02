586e8572502baf294015d9444c30b495d0d81fa9
slekrem <noreply@opendexter.dev>
2026-08-02 01:07:20 -0400

    Cache XG Mobile support and toggle state

--- HandheldCompanion/Commands/Functions/Performance/RogGPU.cs
+++ HandheldCompanion/Commands/Functions/Performance/RogGPU.cs
@@ -11,6 +11,9 @@ namespace HandheldCompanion.Commands.Functions.Performance
     [Serializable]
     public class RogGPU : FunctionCommands
     {
+        private static bool? xgMobileSupported;
+        private bool isToggled;
+
         public RogGPU()
         {
             Name = Properties.Resources.Hotkey_XGMobile;
@@ -37,39 +40,63 @@ namespace HandheldCompanion.Commands.Functions.Performance
             "RC73XA", // ROG Xbox Ally X
         };
 
-        private static bool IsSupported
+        private static bool IsSupported()
         {
-            get
+            if (xgMobileSupported.HasValue)
+                return xgMobileSupported.Value && AsusACPI.Open();
+
+            IDevice device = IDevice.GetCurrent();
+
+            if (!device.ManufacturerName.Equals("ASUSTEK COMPUTER INC.", StringComparison.OrdinalIgnoreCase) ||
+                IncompatibleAsusProducts.Contains(device.ProductName))
             {
-                IDevice device = IDevice.GetCurrent();
-
-                if (!device.ManufacturerName.Equals("ASUSTEK COMPUTER INC.", StringComparison.OrdinalIgnoreCase) ||
-                    IncompatibleAsusProducts.Contains(device.ProductName))
-                    return false;
-
-                if (device.Capabilities.HasFlag(DeviceCapabilities.XGMobile))
-                    return AsusACPI.Open();
-
-                // The XG Mobile ACPI endpoint is present on every compatible ASUS host,
-                // including Flow laptops which currently use DefaultDevice in HC.
-                bool isSupported = AsusACPI.Open() && AsusACPI.IsSupported(AsusACPI.GPUXG);
-                if (isSupported)
-                    device.Capabilities |= DeviceCapabilities.XGMobile;
-
-                return isSupported;
+                xgMobileSupported = false;
+                return false;
             }
+
+            if (device.Capabilities.HasFlag(DeviceCapabilities.XGMobile))
+            {
+                xgMobileSupported = true;
+                return AsusACPI.Open();
+            }
+
+            if (!AsusACPI.Open())
+                return false;
+
+            // The XG Mobile ACPI endpoint is present on every compatible ASUS host,
+            // including Flow laptops which currently use DefaultDevice in HC.
+            xgMobileSupported = AsusACPI.IsSupported(AsusACPI.GPUXG);
+            if (xgMobileSupported.Value)
+                device.Capabilities |= DeviceCapabilities.XGMobile;
+
+            return xgMobileSupported.Value;
         }
 
-        public override bool IsToggled => IsSupported && AsusACPI.DeviceGet(AsusACPI.GPUXG) == 1;
+        private static bool TryGetState(out bool toggled)
+        {
+            toggled = false;
+            if (!IsSupported())
+                return false;
+
+            int state = AsusACPI.DeviceGet(AsusACPI.GPUXG);
+            if (state < 0)
+                return false;
+
+            toggled = state == 1;
+            return true;
+        }
+
+        public override bool IsToggled => isToggled;
 
         public override void Execute(bool IsKeyDown, bool IsKeyUp, bool IsBackground)
         {
-            if (IsSupported)
+            if (TryGetState(out bool toggled))
             {
-                bool enable = !IsToggled;
+                bool enable = !toggled;
 
                 if (!enable) XGM.Reset();
-                AsusACPI.SetXGMode(enable);
+                if (AsusACPI.DeviceSet(AsusACPI.GPUXG, enable ? 1 : 0) == 0)
+                    isToggled = enable;
                 if (enable) XGM.Init();
             }
 
@@ -78,7 +105,8 @@ namespace HandheldCompanion.Commands.Functions.Performance
 
         public void Update(HIDmode profileMode = HIDmode.NotSelected)
         {
-            IsEnabled = IsSupported;
+            IsEnabled = TryGetState(out bool toggled);
+            isToggled = IsEnabled && toggled;
 
             base.Update();
         }
